f9ef1cfa71317e42036c3b25188ad54f8ce2c551
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-17 21:09:13 +0200

    refactoring

--- Module/client.go
+++ Module/client.go
@@ -5,8 +5,8 @@ import (
 	"Systemge/Utilities"
 )
 
-func NewClient(name string, port string, loggerPath string, newApplicationFunc NewApplicationFunc, args []string) *Client.Client {
-	client := Client.New(name, port, Utilities.NewLogger(loggerPath), nil)
+func NewClient(name string, resolverPort string, loggerPath string, newApplicationFunc NewApplicationFunc, args []string) *Client.Client {
+	client := Client.New(name, resolverPort, Utilities.NewLogger(loggerPath), nil)
 	application, err := newApplicationFunc(client, args)
 	if err != nil {
 		panic(err)
