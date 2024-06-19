7583ed942aabd465f62de6d79aa134cecd1e225b
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-19 07:50:15 +0200

    fix

--- Client/brokerMessagingSync.go
+++ Client/brokerMessagingSync.go
@@ -8,7 +8,7 @@ import (
 // resolves the broker address for the provided topic and sends the sync message to the broker responsible for the topic and waits for a response.
 func (client *Client) SyncMessage(topic, origin, payload string) (*Message.Message, error) {
 	message := Message.NewSync(topic, origin, payload)
-	if !client.IsStarted() {
+	if !client.isStarted {
 		return nil, Utilities.NewError("Client not started", nil)
 	}
 	if message.GetSyncRequestToken() == "" {
