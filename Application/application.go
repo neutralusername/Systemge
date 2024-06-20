b843fd288f03cd0cc7e5eb33724cee339bad662f
fabian <fabian@fabian.fabian>
2024-06-20 12:21:12 +0200

    comments

--- Application/application.go
+++ Application/application.go
@@ -4,15 +4,32 @@ import (
 	"Systemge/Message"
 )
 
+// Application is an interface that defines the methods that an application must implement.
+// Every Client must have an Application to Start.
 type Application interface {
+	// GetAsyncMessageHandlers returns a map of message types and their corresponding handlers.
 	GetAsyncMessageHandlers() map[string]AsyncMessageHandler
+
+	// GetSyncMessageHandlers returns a map of message types and their corresponding handlers.
 	GetSyncMessageHandlers() map[string]SyncMessageHandler
+
+	// GetCustomCommandHandlers returns a map of custom commands and their corresponding handlers.
 	GetCustomCommandHandlers() map[string]CustomCommandHandler
+
+	// OnStart is called when the application is started.
+	// If this function returns an error, the application will not be started.
 	OnStart() error
+
+	// OnStop is called when the application is stopped.
+	// If this function returns an error, the application will not be stopped.
 	OnStop() error
 }
+
+// AsyncMessageHandler is a function that takes a message and returns an error.
 type AsyncMessageHandler func(*Message.Message) error
 
-// SyncMessageHandler is a function that takes a message and returns a response payload and an error
+// SyncMessageHandler is a function that takes a message and returns a response payload and an error.
 type SyncMessageHandler func(*Message.Message) (string, error)
+
+// CustomCommandHandler is a function that takes a slice of strings and returns an error.
 type CustomCommandHandler func([]string) error
--- Application/websocketApplication.go
+++ Application/websocketApplication.go
@@ -5,10 +5,27 @@ import (
 	"Systemge/WebsocketClient"
 )
 
+// WebsocketApplication is an interface that defines the methods that a websocket application must implement.
+// Every Client can optionally have a WebsocketApplication which will be started automatically when its Client is started.
 type WebsocketApplication interface {
 	Application
+
+	// GetWebsocketMessageHandlers returns a map of message types and their corresponding handlers.
 	GetWebsocketMessageHandlers() map[string]WebsocketMessageHandler
+
+	// OnConnectHandler is called when a new websocket client connects.
+	// The websocket client is passed as an argument.
+	// Network communication through the Client is possible in this function.
+	// This function is intended for Authentication, Authorization, and Initialization.
 	OnConnectHandler(*WebsocketClient.Client)
+
+	// OnDisconnectHandler is called when a websocket client disconnects.
+	// The websocket client is passed as an argument.
+	// Network communication through the Client is possible in this function.
+	// Communication with the disconnected client is no longer possible.
+	// This function is intended for Cleanup and Logging.
 	OnDisconnectHandler(*WebsocketClient.Client)
 }
+
+// WebsocketMessageHandler is a function that takes a websocket client, which sent the message, and the message itself and returns an error.
 type WebsocketMessageHandler func(*WebsocketClient.Client, *Message.Message) error
