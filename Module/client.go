77f66bad7e1874e141a595811956ba4cc984cedf
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-25 21:30:25 +0200

    removed outdated comment

--- Module/client.go
+++ Module/client.go
@@ -4,10 +4,6 @@ import (
 	"Systemge/Client"
 )
 
-// ClientConfig is a struct that holds the configuration for a client.
-// HTTPCert, HTTPKey, WebsocketCert, and WebsocketKey are paths to the certificate and key files which can be left empty if the server does not use TLS.
-// WebsocketPattern is the pattern that the websocket server will listen to for websocket connections.
-
 // equivalent to Client.New
 func NewClient(config *Client.Config, application Client.Application, httpApplication Client.HTTPApplication, websocketApplication Client.WebsocketApplication) *Client.Client {
 	return Client.New(config, application, httpApplication, websocketApplication)
