d6805dca445e091e86cd5d80f3f42725bc6e1d07
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-25 21:29:47 +0200

    removed outdated comment

--- Module/module.go
+++ Module/module.go
@@ -4,8 +4,5 @@ type Module interface {
 	Start() error
 	Stop() error
 
-	// HandleCustomCommand handles a custom command from the command-line interface
-	// The first argument is the command, the second argument is a slice of arguments
-	// May return an error if the command is not found or if there is an error executing the command
 	GetCustomCommandHandlers() map[string]func([]string) error
 }
