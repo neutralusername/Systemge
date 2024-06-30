b91a1b6e405ba6781bf647678390715b4226fcc1
fabian thomas stuck <stuck.fabian@gmail.com>
2024-06-30 17:54:17 +0200

    commands for spawner

--- Spawner/customCommandHandlers.go
+++ Spawner/customCommandHandlers.go
@@ -1,11 +1,42 @@
 package Spawner
 
-import "Systemge/Node"
+import (
+	"Systemge/Error"
+	"Systemge/Node"
+)
 
 func (spawner *Spawner) GetCustomCommandHandlers() map[string]Node.CustomCommandHandler {
 	return map[string]Node.CustomCommandHandler{
-		"command": func(node *Node.Node, args []string) error {
-			println("executing command")
+		"spawnedNodes": func(node *Node.Node, args []string) error {
+			spawner.mutex.Lock()
+			defer spawner.mutex.Unlock()
+			for _, spawnedNode := range spawner.spawnedNodes {
+				println(spawnedNode.GetName())
+			}
+			return nil
+		},
+		"spawnNode": func(node *Node.Node, args []string) error {
+			if len(args) != 1 {
+				return Error.New("Invalid number of arguments", nil)
+			}
+			spawner.mutex.Lock()
+			defer spawner.mutex.Unlock()
+			err := spawner.StartNode(node, args[0])
+			if err != nil {
+				return Error.New("Error starting node "+args[0], err)
+			}
+			return nil
+		},
+		"endNode": func(node *Node.Node, args []string) error {
+			if len(args) != 1 {
+				return Error.New("Invalid number of arguments", nil)
+			}
+			spawner.mutex.Lock()
+			defer spawner.mutex.Unlock()
+			err := spawner.EndNode(node, args[0])
+			if err != nil {
+				return Error.New("Error ending node "+args[0], err)
+			}
 			return nil
 		},
 	}
