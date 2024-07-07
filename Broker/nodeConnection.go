1ad9e6515b86dec0eb1933b736dbe6ad2dd76282
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-07 19:29:12 +0200

    fixed deadlock

--- Broker/nodeConnection.go
+++ Broker/nodeConnection.go
@@ -51,12 +51,13 @@ func (nodeConnection *nodeConnection) receive() ([]byte, error) {
 
 func (broker *Broker) disconnect(nodeConnection *nodeConnection) {
 	broker.operationMutex.Lock()
-	defer broker.operationMutex.Unlock()
 	if nodeConnection.netConn == nil {
+		broker.operationMutex.Unlock()
 		return
 	}
 	nodeConnection.netConn.Close()
 	nodeConnection.netConn = nil
+	broker.operationMutex.Unlock()
 	err := broker.removeNodeConnection(nodeConnection)
 	if err != nil {
 		broker.logger.Log(Error.New("Error removing node \""+nodeConnection.name+"\"", err).Error())
