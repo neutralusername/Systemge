48ba80685364b5219fa333013b5627f08d63f6a7
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-14 11:01:17 +0200

    added logs for callback

--- Oauth2/http.go
+++ Oauth2/http.go
@@ -16,9 +16,10 @@ type oauth2SessionRequest struct {
 
 func (server *Server) oauth2Callback() Http.RequestHandler {
 	return func(responseWriter http.ResponseWriter, httpRequest *http.Request) {
+		server.config.Logger.Info("oauth2 callback for " + httpRequest.RemoteAddr)
 		state := httpRequest.FormValue("state")
 		if state != server.config.Oauth2State {
-			server.config.Logger.Warning(Error.New("oauth2 state mismatch", nil).Error())
+			server.config.Logger.Warning(Error.New("failed oauth2 state check", nil).Error())
 			http.Redirect(responseWriter, httpRequest, server.config.FailureCallbackRedirect, http.StatusMovedPermanently)
 			return
 		}
@@ -37,16 +38,18 @@ func (server *Server) oauth2Callback() Http.RequestHandler {
 		server.sessionRequestChannel <- Oauth2TokenRequest
 		sessionId := <-sessionIdChannel
 		if sessionId == "" {
+			server.config.Logger.Warning(Error.New("failed creating session", nil).Error())
 			http.Redirect(responseWriter, httpRequest, server.config.FailureCallbackRedirect, http.StatusMovedPermanently)
 			return
 		}
+		server.config.Logger.Info("oauth2 callback success for " + httpRequest.RemoteAddr)
 		http.Redirect(responseWriter, httpRequest, server.config.SucessCallbackRedirect+"?sessionId="+sessionId, http.StatusMovedPermanently)
 	}
 }
 
 func (server *Server) oauth2Auth() Http.RequestHandler {
 	return func(responseWriter http.ResponseWriter, httpRequest *http.Request) {
-		server.config.Logger.Info("auth request by " + httpRequest.RemoteAddr)
+		server.config.Logger.Info("oauth2 request by " + httpRequest.RemoteAddr)
 		url := server.config.OAuth2Config.AuthCodeURL(server.config.Oauth2State)
 		http.Redirect(responseWriter, httpRequest, url, http.StatusTemporaryRedirect)
 	}
