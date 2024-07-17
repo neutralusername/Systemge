87b26f54538575cc93284c372c552cbf9723a717
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-17 14:58:44 +0200

    refactoring

--- Oauth2/server.go
+++ Oauth2/server.go
@@ -24,6 +24,25 @@ type Server struct {
 	isStarted   bool
 }
 
+func New(config Config.Oauth2) (*Server, error) {
+	if config.Randomizer == nil {
+		config.Randomizer = Utilities.NewRandomizer(Utilities.GetSystemTime())
+	}
+	if config.TokenHandler == nil {
+		return nil, Error.New("TokenHandler is required", nil)
+	}
+	if config.OAuth2Config == nil {
+		return nil, Error.New("OAuth2Config is required", nil)
+	}
+	server := &Server{
+		sessionRequestChannel: make(chan *oauth2SessionRequest),
+		config:                &config,
+		sessions:              make(map[string]*session),
+		identities:            make(map[string]*session),
+	}
+	return server, nil
+}
+
 func (server *Server) Start() error {
 	server.mutex.Lock()
 	defer server.mutex.Unlock()
