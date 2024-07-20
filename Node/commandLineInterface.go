eba13985ee685305352dc48df0f867b102aba552
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-20 11:31:44 +0200

    specific nodes can now be selected for commands using @<nodeName>

--- Node/commandLineInterface.go
+++ Node/commandLineInterface.go
@@ -18,6 +18,26 @@ type Schedule struct {
 	repeat      bool
 	args        []string
 	command     string
+	nodeNames   []string
+}
+
+func processSegments(inputSegments []string) (nodeNames []string, reverse bool, command string, args []string) {
+	nodeNames = []string{}
+	for _, segment := range inputSegments {
+		if segment[0] != '@' {
+			break
+		}
+		nodeNames = append(nodeNames, segment[1:])
+	}
+	inputSegments = inputSegments[len(nodeNames):]
+	reverse = false
+	if inputSegments[0][0] == '!' {
+		inputSegments[0] = inputSegments[0][1:]
+		reverse = true
+	}
+	command = inputSegments[0]
+	args = inputSegments[1:]
+	return
 }
 
 // starts a command-line interface for the provided nodes
@@ -41,78 +61,50 @@ func StartCommandLineInterface(nodes ...*Node) {
 		}
 		input = strings.Trim(input, "\r\n")
 		inputSegments := strings.Split(input, " ")
-		if inputSegments[0] == "" || inputSegments[0] == "!" {
+		if inputSegments[0] == "" || inputSegments[0] == "!" || inputSegments[0] == "@" {
 			continue
 		}
-		reverse := false
-		if inputSegments[0][0] == '!' {
-			inputSegments[0] = inputSegments[0][1:]
-			reverse = true
-		}
-		switch inputSegments[0] {
+		nodeNames, reverse, command, args := processSegments(inputSegments)
+		switch command {
 		case "?":
 			println("> commands from the custom command handlers of nodes can be executed in this command line interface")
-			println("> type <availableCommands> to see the available commands for each node")
-			println("> precede any command with an exclamation mark <!command> to reverse the order of nodes for the command")
+			println("> type <listNodes> to see the available nodes")
+			println("> type <listCommands> to see the available commands for each node")
+			println("> precede any command with a variable amount of <@nodeName> to execute the command on specific nodes")
+			println("> precede any command with an exclamation mark (<!command>) to reverse the order of nodes for the command")
 			println("> schedule a command to be executed at a later time")
 			println("> after starting a schedule its scheduleId will be returned which can be used to stop the schedule")
 			println("> startSchedule <command> <time in ms> <repeat> <args...>")
-			println("> stopSchedule <scheduleId>")
-			println("> listSchedules")
 			println("> restart stops all nodes and then starts them again in the order they were provided (! affects only the stop part)")
 		case "exit":
 			return
-		case "startSchedule":
-			startSchedule(inputSegments, schedules, nodes...)
-		case "availableCommands":
+		case "listCommands":
 			for _, node := range nodes {
 				println("node: \"" + node.GetName() + "\"")
 				for command := range node.GetCommandHandlers() {
 					println("\t\"" + command + "\"")
 				}
 			}
+			println("schedule commands: startSchedule, stopSchedule, listSchedules")
+			println("system commands: listNodes, restart")
+		case "listNodes":
+			for _, node := range nodes {
+				println("node: \"" + node.GetName() + "\"")
+			}
+		case "startSchedule":
+			startSchedule(inputSegments, schedules, nodes...)
 		case "stopSchedule":
 			stopSchedule(inputSegments, schedules)
 		case "listSchedules":
 			for scheduleId, schedule := range schedules {
-				fmt.Println("scheduleId: \"" + scheduleId + "\", command: \"" + schedule.command + "\" time started: " + schedule.timeStarted.String() + ", duration: " + schedule.duration.String() + ", repeat: " + fmt.Sprint(schedule.repeat) + ", args: " + strings.Join(schedule.args, " "))
+				fmt.Println("scheduleId: \"" + scheduleId + "\", command: \"" + schedule.command + "\" time started: " + schedule.timeStarted.String() + ", duration: " + schedule.duration.String() + ", repeat: " + fmt.Sprint(schedule.repeat) + ", nodeNames: " + fmt.Sprint(schedule.nodeNames) + ", args: " + fmt.Sprint(schedule.args))
 			}
 		case "restart":
-			restart(reverse, nodes...)
+			restart(nodeNames, reverse, nodes...)
 		default:
-			handleCommands(reverse, inputSegments, nodes...)
+			handleCommands(nodeNames, reverse, command, args, nodes...)
 		}
 	}
-}
-
-func restart(reverse bool, nodes ...*Node) {
-	if reverse {
-		for i := len(nodes) - 1; i >= 0; i-- {
-			executeCommand(nodes[i], "stop", nil)
-		}
-	} else {
-		for _, node := range nodes {
-			executeCommand(node, "stop", nil)
-		}
-	}
-	for _, node := range nodes {
-		executeCommand(node, "start", nil)
-	}
-}
-
-func stopSchedule(inputSegments []string, schedules map[string]*Schedule) {
-	if len(inputSegments) < 2 {
-		println("invalid number of arguments")
-		return
-	}
-	command := inputSegments[1]
-	schedule := schedules[command]
-	if schedule == nil {
-		println("schedule not found")
-		return
-	}
-	schedule.timer.Stop()
-	delete(schedules, command)
 }
 
 func startSchedule(inputSegments []string, schedules map[string]*Schedule, nodes ...*Node) {
@@ -123,16 +115,19 @@ func startSchedule(inputSegments []string, schedules map[string]*Schedule, nodes
 	if schedules[inputSegments[1]] != nil {
 		stopSchedule([]string{"stopSchedule", inputSegments[1]}, schedules)
 	}
-	command := inputSegments[1]
-	timeMs := Helpers.StringToUint64(inputSegments[2])
-	repeat := inputSegments[3] == "true"
-	args := inputSegments[4:]
+	nodeNames, reverse, _, args := processSegments(inputSegments)
+	command := args[0]
+	timeMs := Helpers.StringToInt(args[1])
+	repeat := args[2] == "true"
+	args = args[3:]
+	fmt.Println(nodeNames, reverse, command, args)
 	schedule := &Schedule{
 		timeStarted: time.Now(),
 		duration:    time.Duration(timeMs) * time.Millisecond,
 		repeat:      repeat,
 		args:        args,
 		command:     command,
+		nodeNames:   nodeNames,
 	}
 	scheduleId := Tools.RandomString(10, Tools.ALPHA_NUMERIC)
 	for schedules[scheduleId] != nil {
@@ -141,16 +136,10 @@ func startSchedule(inputSegments []string, schedules map[string]*Schedule, nodes
 	schedules[scheduleId] = schedule
 	println("schedule started with id \"" + scheduleId + "\"")
 	schedule.timer = time.AfterFunc(schedule.duration, func() {
-		reverse := false
-		c := command
-		if command[0] == '!' {
-			reverse = true
-			c = command[1:]
-		}
-		if c == "restart" {
-			restart(reverse, nodes...)
+		if command == "restart" {
+			restart(nodeNames, reverse, nodes...)
 		} else {
-			handleCommands(reverse, append([]string{c}, args...), nodes...)
+			handleCommands(nodeNames, reverse, command, args, nodes...)
 		}
 		if schedule.repeat {
 			schedule.timer.Reset(schedule.duration)
@@ -160,19 +149,84 @@ func startSchedule(inputSegments []string, schedules map[string]*Schedule, nodes
 	})
 }
 
-func handleCommands(reverse bool, inputSegments []string, nodes ...*Node) bool {
-	commandExecuted := false
+func restart(nodeNames []string, reverse bool, nodes ...*Node) {
 	if reverse {
-		for i := len(nodes) - 1; i >= 0; i-- {
-			executeCommand(nodes[i], inputSegments[0], &commandExecuted, inputSegments[1:]...)
+		if len(nodeNames) > 0 {
+			for i := len(nodeNames) - 1; i >= 0; i-- {
+				for _, node := range nodes {
+					if node.GetName() == nodeNames[i] {
+						executeCommand(node, "stop", nil)
+					}
+				}
+			}
+		} else {
+			for i := len(nodes) - 1; i >= 0; i-- {
+				executeCommand(nodes[i], "stop", nil)
+			}
+		}
+	} else {
+		if len(nodeNames) > 0 {
+			for _, nodeName := range nodeNames {
+				for _, node := range nodes {
+					if node.GetName() == nodeName {
+						executeCommand(node, "stop", nil)
+					}
+				}
+			}
+		} else {
+			for _, node := range nodes {
+				executeCommand(node, "stop", nil)
+			}
+		}
+	}
+	if len(nodeNames) > 0 {
+		for _, nodeName := range nodeNames {
+			for _, node := range nodes {
+				if node.GetName() == nodeName {
+					executeCommand(node, "start", nil)
+				}
+			}
 		}
 	} else {
 		for _, node := range nodes {
-			executeCommand(node, inputSegments[0], &commandExecuted, inputSegments[1:]...)
+			executeCommand(node, "start", nil)
+		}
+	}
+}
+
+func handleCommands(nodeNames []string, reverse bool, command string, args []string, nodes ...*Node) bool {
+	commandExecuted := false
+	if reverse {
+		if len(nodeNames) > 0 {
+			for i := len(nodeNames) - 1; i >= 0; i-- {
+				for _, node := range nodes {
+					if node.GetName() == nodeNames[i] {
+						executeCommand(node, command, &commandExecuted, args...)
+					}
+				}
+			}
+		} else {
+			for i := len(nodes) - 1; i >= 0; i-- {
+				executeCommand(nodes[i], command, &commandExecuted, args...)
+			}
+		}
+	} else {
+		if len(nodeNames) > 0 {
+			for _, nodeName := range nodeNames {
+				for _, node := range nodes {
+					if node.GetName() == nodeName {
+						executeCommand(node, command, &commandExecuted, args...)
+					}
+				}
+			}
+		} else {
+			for _, node := range nodes {
+				executeCommand(node, command, &commandExecuted, args...)
+			}
 		}
 	}
 	if !commandExecuted {
-		println("command not found \"" + inputSegments[0] + "\"")
+		println("command not found \"" + command + "\"")
 	}
 	return true
 }
@@ -190,4 +244,19 @@ func executeCommand(node *Node, command string, commandExecuted *bool, args ...s
 			println(err.Error())
 		}
 	}
+}
+
+func stopSchedule(inputSegments []string, schedules map[string]*Schedule) {
+	if len(inputSegments) < 2 {
+		println("invalid number of arguments")
+		return
+	}
+	command := inputSegments[1]
+	schedule := schedules[command]
+	if schedule == nil {
+		println("schedule not found")
+		return
+	}
+	schedule.timer.Stop()
+	delete(schedules, command)
 }
