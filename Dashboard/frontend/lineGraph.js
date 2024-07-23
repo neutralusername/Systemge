cd0b526872dc99d783c53c4a227f3a2e0379afaa
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-23 08:00:45 +0200

    refactoring using react lifecycle-methods

--- Dashboard/frontend/lineGraph.js
+++ Dashboard/frontend/lineGraph.js
@@ -5,52 +5,55 @@ export class lineGraph extends React.Component {
 			chart : null,
 		}
 	}
-	render() {
-		if (document.getElementById(this.props.chartName) !== null) {
-			if ( this.state.chart === null) {
-				let options = this.props.options;
-				if (this.props.options === undefined) {
-					options = {
-						responsive: false,
-						maintainAspectRatio: true,
-						scales: {
-							y: {
-								beginAtZero: false,
-							},
+	componentDidMount() {
+		if ( this.state.chart === null) {
+			let options = this.props.options;
+			if (this.props.options === undefined) {
+				options = {
+					responsive: false,
+					maintainAspectRatio: true,
+					scales: {
+						y: {
+							beginAtZero: false,
 						},
-						animation: false,
-						interaction: false,
-					}
+					},
+					animation: false,
+					interaction: false,
 				}
-				let fill = this.props.fill;
-				if (this.props.fill === undefined) {
-					fill = true;
-				}
-				let graphColor = this.props.graphColor;
-				if (this.props.graphColor === undefined) {
-					graphColor = "rgb(75, 192, 192)";
-				}
-				this.setState({
-					chart : new Chart(document.getElementById(this.props.chartName).getContext('2d'), {
-						type: 'line',
-						data: {
-							labels: Object.keys(this.props.dataSet),
-							datasets: [{
-								label: this.props.dataLabel,
-								data: Object.values(this.props.dataSet),
-								fill: fill,
-								borderColor: graphColor,
-							}],
-						},
-						options: options,
-					}),
-				});
-			} else {
-				this.state.chart.data.labels = Object.keys(this.props.dataSet);
-				this.state.chart.data.datasets[0].data = Object.values(this.props.dataSet);
-				this.state.chart.update();
 			}
+			let fill = this.props.fill;
+			if (this.props.fill === undefined) {
+				fill = true;
+			}
+			let graphColor = this.props.graphColor;
+			if (this.props.graphColor === undefined) {
+				graphColor = "rgb(75, 192, 192)";
+			}
+			this.setState({
+				chart : new Chart(this.props.chartName, {
+					type: 'line',
+					data: {
+						labels: Object.keys(this.props.dataSet),
+						datasets: [{
+							label: this.props.dataLabel,
+							data: Object.values(this.props.dataSet),
+							fill: fill,
+							borderColor: graphColor,
+						}],
+					},
+					options: options,
+				}),
+			});
+		} 
+	}
+	componentDidUpdate() {
+		if (this.state.chart !== null) {
+			this.state.chart.data.labels = Object.keys(this.props.dataSet);
+			this.state.chart.data.datasets[0].data = Object.values(this.props.dataSet);
+			this.state.chart.update();
 		}
+	}
+	render() {
 		return 	React.createElement(
 			"canvas", {
 				id: this.props.chartName,
