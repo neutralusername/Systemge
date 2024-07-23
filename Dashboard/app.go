b5cc5486e14882a49350dd5e646c2140ce05b1de
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-23 18:03:27 +0200

    refactoring

--- Dashboard/app.go
+++ Dashboard/app.go
@@ -22,7 +22,7 @@ func New(config *Config.Dashboard, nodes ...*Node.Node) *App {
 		nodes:  make(map[string]*Node.Node),
 		config: config,
 	}
-	_, filePath, _, _ := runtime.Caller(0)
+
 	app.httpMessageHandlers = map[string]http.HandlerFunc{}
 	for _, node := range nodes {
 		app.nodes[node.GetName()] = node
@@ -32,38 +32,44 @@ func New(config *Config.Dashboard, nodes ...*Node.Node) *App {
 				panic(err)
 			}
 		}
-		app.httpMessageHandlers["/"+node.GetName()] = func(w http.ResponseWriter, r *http.Request) {
-			http.StripPrefix("/"+node.GetName(), http.FileServer(http.Dir(filePath[:len(filePath)-len("app.go")]+"frontend"))).ServeHTTP(w, r)
-		}
-		app.httpMessageHandlers["/"+node.GetName()+"/start"] = func(w http.ResponseWriter, r *http.Request) {
-			err := node.Start()
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
-			w.WriteHeader(http.StatusOK)
-		}
-		app.httpMessageHandlers["/"+node.GetName()+"/stop"] = func(w http.ResponseWriter, r *http.Request) {
-			err := node.Stop()
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
-			w.Write([]byte("OK"))
-		}
-		app.httpMessageHandlers["/"+node.GetName()+"/command/"] = func(w http.ResponseWriter, r *http.Request) {
-			args := r.URL.Path[len("/"+node.GetName()+"/command/"):]
-			argsSplit := strings.Split(args, " ")
-			if len(argsSplit) == 0 {
-				http.Error(w, "No command", http.StatusBadRequest)
-				return
-			}
-			result, err := app.nodeCommand(&Command{Name: node.GetName(), Command: argsSplit[0], Args: argsSplit[1:]})
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			w.Write([]byte(result))
-		}
+		app.registerNodeHttpHandlers(node)
 	}
+	_, filePath, _, _ := runtime.Caller(0)
 	app.httpMessageHandlers["/"] = HTTP.SendDirectory(filePath[:len(filePath)-len("app.go")] + "frontend")
 	return app
+}
+
+func (app *App) registerNodeHttpHandlers(node *Node.Node) {
+	_, filePath, _, _ := runtime.Caller(0)
+	app.httpMessageHandlers["/"+node.GetName()] = func(w http.ResponseWriter, r *http.Request) {
+		http.StripPrefix("/"+node.GetName(), http.FileServer(http.Dir(filePath[:len(filePath)-len("app.go")]+"frontend"))).ServeHTTP(w, r)
+	}
+	app.httpMessageHandlers["/"+node.GetName()+"/start"] = func(w http.ResponseWriter, r *http.Request) {
+		err := node.Start()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		w.WriteHeader(http.StatusOK)
+	}
+	app.httpMessageHandlers["/"+node.GetName()+"/stop"] = func(w http.ResponseWriter, r *http.Request) {
+		err := node.Stop()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		w.Write([]byte("OK"))
+	}
+	app.httpMessageHandlers["/"+node.GetName()+"/command/"] = func(w http.ResponseWriter, r *http.Request) {
+		args := r.URL.Path[len("/"+node.GetName()+"/command/"):]
+		argsSplit := strings.Split(args, " ")
+		if len(argsSplit) == 0 {
+			http.Error(w, "No command", http.StatusBadRequest)
+			return
+		}
+		result, err := app.nodeCommand(&Command{Name: node.GetName(), Command: argsSplit[0], Args: argsSplit[1:]})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte(result))
+	}
 }
