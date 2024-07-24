7cc701ab86303bdd712e1583b43b3420060e554b
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-24 12:16:34 +0200

    updated chart colors

--- Dashboard/frontend/root.js
+++ Dashboard/frontend/root.js
@@ -30,19 +30,19 @@ export class root extends React.Component {
         this.counterConfig = {
             nodeResolverCounters: {
                 labels: ["configRequests", "resolutionRequests", "bytesSent", "bytesReceived"],
-                colors: ["rgb(75, 192, 192)", "rgb(192, 75, 192)", "rgb(192, 192, 75)", "rgb(75, 192, 75)"],
+                colors: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 206, 86)", "rgb(75, 192, 192)"],
             },
             nodeBrokerCounters: {
                 labels: ["incomingMessages", "outgoingMessages", "configRequests", "bytesSent", "bytesReceived"],
-                colors: ["rgb(75, 192, 192)", "rgb(192, 75, 192)", "rgb(192, 192, 75)", "rgb(75, 192, 75)", "rgb(75, 75, 192)"],
+                colors: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 206, 86)", "rgb(75, 192, 192)", "rgb(153, 102, 255)"],
             },
             nodeWebsocketCounters: {
                 labels: ["inc", "out", "clientCount", "groupCount", "bytesSent", "bytesReceived"],
-                colors: ["rgb(75, 192, 192)", "rgb(192, 75, 192)", "rgb(192, 192, 75)", "rgb(75, 192, 75)", "rgb(75, 75, 192)", "rgb(192, 75, 75)"],
+                colors: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 206, 86)", "rgb(75, 192, 192)", "rgb(153, 102, 255)", "rgb(255, 159, 64)"],
             },
             nodeSystemgeCounters: {
                 labels: ["incSyncReq", "incSyncRes", "incAsync", "outSyncReq", "outSyncRes", "outAsync", "bytesSent", "bytesReceived"],
-                colors: ["rgb(75, 192, 192)", "rgb(192, 75, 192)", "rgb(192, 192, 75)", "rgb(75, 192, 75)", "rgb(75, 75, 192)", "rgb(192, 75, 75)", "rgb(75, 192, 192)", "rgb(192, 75, 192)"],
+                colors: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 206, 86)", "rgb(75, 192, 192)", "rgb(153, 102, 255)", "rgb(255, 159, 64)", "rgb(75, 192, 192)", "rgb(54, 235, 162)"],
             },
         };
     }
