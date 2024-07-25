74a4621969da1566070a918aee8ff35e8fba5bd4
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-25 20:32:24 +0200

    improved websocket group funcs ( can add savely add more websockets to one group in one operation.. fails if one is not able to join)

--- Node/websocketGroups.go
+++ Node/websocketGroups.go
@@ -2,44 +2,49 @@ package Node
 
 import "Systemge/Error"
 
-func (node *Node) AddToWebsocketGroup(groupId string, websocketId string) error {
+func (node *Node) AddToWebsocketGroup(groupId string, websocketIds ...string) error {
 	if websocket := node.websocket; websocket != nil {
 		node.websocket.mutex.Lock()
 		defer node.websocket.mutex.Unlock()
-		if node.websocket.clients[websocketId] == nil {
-			return Error.New("websocketClient with id "+websocketId+" does not exist", nil)
-		}
-		if node.websocket.clientGroups[websocketId][groupId] {
-			return Error.New("websocketClient with id "+websocketId+" is already in group "+groupId, nil)
+		for _, websocketId := range websocketIds {
+			if node.websocket.clients[websocketId] == nil {
+				return Error.New("websocketClient with id "+websocketId+" does not exist", nil)
+			}
+			if node.websocket.clientGroups[websocketId][groupId] {
+				return Error.New("websocketClient with id "+websocketId+" is already in group "+groupId, nil)
+			}
 		}
 		if node.websocket.groups[groupId] == nil {
 			node.websocket.groups[groupId] = make(map[string]*WebsocketClient)
 		}
-		node.websocket.groups[groupId][websocketId] = node.websocket.clients[websocketId]
-		node.websocket.clientGroups[websocketId][groupId] = true
+		for _, websocketId := range websocketIds {
+			node.websocket.groups[groupId][websocketId] = node.websocket.clients[websocketId]
+			node.websocket.clientGroups[websocketId][groupId] = true
+		}
 		return nil
 	}
 	return Error.New("websocket component is not initialized", nil)
 }
 
-func (node *Node) RemoveFromWebsocketGroup(groupId string, websocketId string) error {
+func (node *Node) RemoveFromWebsocketGroup(groupId string, websocketIds ...string) error {
 	if websocket := node.websocket; websocket != nil {
 		node.websocket.mutex.Lock()
 		defer node.websocket.mutex.Unlock()
 		if node.websocket.groups[groupId] == nil {
 			return Error.New("group with id "+groupId+" does not exist", nil)
 		}
-		if node.websocket.groups[groupId][websocketId] == nil {
-			return Error.New("websocketClient with id "+websocketId+" is not in group "+groupId, nil)
+		for _, websocketId := range websocketIds {
+			if node.websocket.clients[websocketId] == nil {
+				return Error.New("websocketClient with id "+websocketId+" does not exist", nil)
+			}
+			if node.websocket.clientGroups[websocketId][groupId] == false {
+				return Error.New("websocketClient with id "+websocketId+" is not in group "+groupId, nil)
+			}
 		}
-		if node.websocket.clients[websocketId] == nil {
-			return Error.New("websocketClient with id "+websocketId+" does not exist", nil)
+		for _, websocketId := range websocketIds {
+			delete(node.websocket.clientGroups[websocketId], groupId)
+			delete(node.websocket.groups[groupId], websocketId)
 		}
-		if node.websocket.clientGroups[websocketId] == nil {
-			return Error.New("websocketClient with id "+websocketId+" is not in any groups", nil)
-		}
-		delete(node.websocket.clientGroups[websocketId], groupId)
-		delete(node.websocket.groups[groupId], websocketId)
 		if len(node.websocket.groups[groupId]) == 0 {
 			delete(node.websocket.groups, groupId)
 		}
