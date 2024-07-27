a455164d3cbb015a7a88a2977ab9d425d7e334c2
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-27 21:42:47 +0200

    fix

--- Node/systemgeComponent.go
+++ Node/systemgeComponent.go
@@ -110,10 +110,10 @@ func (node *Node) startSystemgeComponent() error {
 func (node *Node) stopSystemgeComponent() error {
 	node.systemge.mutex.Lock()
 	defer node.systemge.mutex.Unlock()
-	node.systemge = nil
 	for _, brokerConnection := range node.systemge.brokerConnections {
 		brokerConnection.closeNetConn()
 		delete(node.systemge.brokerConnections, brokerConnection.endpoint.Address)
 	}
+	node.systemge = nil
 	return nil
 }
