8343f9c016cc8b328b00d59e3c8ab36d59c1eae2
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-27 17:20:25 +0200

    systemge sample updater

---
+++ updateSamples.sh
@@ -0,0 +1,23 @@
+cd ../SystemgeSampleChat/main
+export GOPROXY=direct
+go get -u github.com/neutralusername/Systemge
+
+cd ../../SystemgeSampleConwaysGameOfLife/main
+export GOPROXY=direct
+go get -u github.com/neutralusername/Systemge
+
+cd ../../SystemgeSamplePingPong/main
+export GOPROXY=direct
+go get -u github.com/neutralusername/Systemge
+
+cd ../../SystemgeSamplePingSpawner/main
+export GOPROXY=direct
+go get -u github.com/neutralusername/Systemge
+
+cd ../../SystemgeSampleChessServer/main
+export GOPROXY=direct
+go get -u github.com/neutralusername/Systemge
+
+cd ../../SystemgeSampleDiscordAuth/main
+export GOPROXY=direct
+go get -u github.com/neutralusername/Systemge
