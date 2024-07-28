8d5325a1a793cd18d8ac8119eb0a9c7bd4766aac
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-28 16:32:03 +0200

    refactoring

---
+++ Config/components.go
@@ -0,0 +1,63 @@
+package Config
+
+import "github.com/gorilla/websocket"
+
+type Systemge struct {
+	HandleMessagesSequentially bool // default: false
+
+	BrokerSubscribeDelayMs    uint64 // default: 0 (delay after failed broker subscription attempt)
+	TopicResolutionLifetimeMs int64  // default: <0 = infinite; 0 = resolve every time; >0 = resolve every n ms
+	SyncResponseTimeoutMs     uint64 // default: 0 = sync messages are not supported
+	TcpTimeoutMs              uint64 // default: 0 = block forever
+	MaxSubscribeAttempts      uint64 // default: 0 = infinite
+
+	ResolverEndpoint *TcpEndpoint // *required*
+}
+
+type Websocket struct {
+	Pattern string     // *required*
+	Server  *TcpServer // *required*
+
+	HandleClientMessagesSequentially bool   // default: false
+	ClientMessageCooldownMs          uint64 // default: 0
+	ClientWatchdogTimeoutMs          uint64 // default: 0
+
+	Upgrader *websocket.Upgrader // *required*
+
+}
+
+type HTTP struct {
+	Server *TcpServer // *required*
+}
+
+type Broker struct {
+	Server       *TcpServer   // *required*
+	Endpoint     *TcpEndpoint // *required*
+	ConfigServer *TcpServer   // *required*
+
+	SyncTopics  []string
+	AsyncTopics []string
+
+	ResolverConfigEndpoint *TcpEndpoint // *required*
+
+	SyncResponseTimeoutMs uint64 // default: 0
+	TcpTimeoutMs          uint64 // default: 0 = block forever
+
+	IncomingMessageByteLimit uint64 // default: 0 = unlimited
+	MaxOriginSize            int    // default: 0 = unlimited
+	MaxPayloadSize           int    // default: 0 = unlimited
+	MaxTopicSize             int    // default: 0 = unlimited
+	MaxSyncKeySize           int    // default: 0 = unlimited
+}
+
+type Resolver struct {
+	Server       *TcpServer // *required*
+	ConfigServer *TcpServer // *required*
+
+	TcpTimeoutMs uint64 // default: 0 = block forever
+
+	IncomingMessageByteLimit uint64 // default: 0 = unlimited
+	MaxPayloadSize           int    // default: 0 = unlimited
+	MaxOriginSize            int    // default: 0 = unlimited
+	MaxTopicSize             int    // default: 0 = unlimited
+}
