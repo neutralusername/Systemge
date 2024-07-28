40a3a423b57d48c51364ccc3f206d2738a50d1d0
fabian thomas stuck <stuck.fabian@gmail.com>
2024-07-28 20:55:17 +0200

    refactoring

--- HTTP/http.go
+++ HTTP/http.go
@@ -77,12 +77,12 @@ func (server *Server) Start() error {
 		}
 	}()
 	time.Sleep(10 * time.Millisecond)
+	ended = true
 	select {
 	case err := <-errorChannel:
 		return Error.New("failed to start http server", err)
 	default:
 	}
-	ended = true
 	return nil
 }
 
