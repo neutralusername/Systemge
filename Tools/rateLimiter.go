760c7c89531949a0283a19a56ea074e259e88ded
fabian thomas stuck <stuck.fabian@gmail.com>
2024-08-03 09:52:33 +0200

    added mutex to prevent race conditions on concurrent refill/consume

--- Tools/rateLimiter.go
+++ Tools/rateLimiter.go
@@ -1,6 +1,7 @@
 package Tools
 
 import (
+	"sync"
 	"time"
 
 	"github.com/neutralusername/Systemge/Config"
@@ -12,6 +13,7 @@ type RateLimiter struct {
 	refillRate       uint64
 	refillIntervalMs uint64
 	active           bool
+	mutex            sync.Mutex
 }
 
 func NewRateLimiter(config *Config.RateLimiter) *RateLimiter {
@@ -35,15 +37,19 @@ func (rateLimiter *RateLimiter) Stop() {
 func (rateLimiter *RateLimiter) refillRoutine() {
 	time.Sleep(time.Duration(rateLimiter.refillIntervalMs) * time.Millisecond)
 	for rateLimiter.active {
+		rateLimiter.mutex.Lock()
 		rateLimiter.bucket += rateLimiter.refillRate
 		if rateLimiter.bucket > rateLimiter.maxBucketSize {
 			rateLimiter.bucket = rateLimiter.maxBucketSize
 		}
+		rateLimiter.mutex.Unlock()
 		time.Sleep(time.Duration(rateLimiter.refillIntervalMs) * time.Millisecond)
 	}
 }
 
 func (rateLimiter *RateLimiter) Consume(amount uint64) bool {
+	rateLimiter.mutex.Lock()
+	defer rateLimiter.mutex.Unlock()
 	if rateLimiter.bucket < amount {
 		return false
 	}
