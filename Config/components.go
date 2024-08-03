8257ea9b7a66bd4a56f88436e262ee1ca21d8964
fabian thomas stuck <stuck.fabian@gmail.com>
2024-08-03 15:29:25 +0200

    refactoring

--- Config/components.go
+++ Config/components.go
@@ -67,7 +67,7 @@ type HTTP struct {
 	MaxHeaderBytes      int        `json:"maxHeaderBytes"`      // default: 0 == 1 MB
 	ReadHeaderTimeoutMs int        `json:"readHeaderTimeoutMs"` // default: 0 (no timeout)
 	WriteTimeoutMs      int        `json:"writeTimeoutMs"`      // default: 0 (no timeout)
-	MaxBodyBytes        int        `json:"maxBodyBytes"`        // default: 0 == 1 MB
+	MaxBodyBytes        int64      `json:"maxBodyBytes"`        // default: 0 == 1 MB
 }
 
 func UnmarshalHTTP(data string) *HTTP {
--- Node/httpComponent.go
+++ Node/httpComponent.go
@@ -40,7 +40,7 @@ func (httpComponent *httpComponent) httpRequestWrapper(handler func(w http.Respo
 	return func(w http.ResponseWriter, r *http.Request) {
 		httpComponent.requestCounter.Add(1)
 		if httpComponent.config.MaxBodyBytes > 0 {
-			r.Body = http.MaxBytesReader(w, r.Body, int64(httpComponent.config.MaxBodyBytes))
+			r.Body = http.MaxBytesReader(w, r.Body, httpComponent.config.MaxBodyBytes)
 		}
 		ip, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
