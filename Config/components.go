5f4017ca387967f0e806975954694a6bcf878871
fabian thomas stuck <stuck.fabian@gmail.com>
2024-08-03 16:32:40 +0200

    comment to clarify behaviour

--- Config/components.go
+++ Config/components.go
@@ -64,7 +64,7 @@ func UnmarshalWebsocket(data string) *Websocket {
 
 type HTTP struct {
 	ServerConfig        *TcpServer `json:"serverConfig"`        // *required*
-	MaxHeaderBytes      int        `json:"maxHeaderBytes"`      // default: 0 == 1 MB
+	MaxHeaderBytes      int        `json:"maxHeaderBytes"`      // default: <=0 == 1 MB (whichever value you choose, golangs http package will add 4096 bytes on top of it....)
 	ReadHeaderTimeoutMs int        `json:"readHeaderTimeoutMs"` // default: 0 (no timeout)
 	WriteTimeoutMs      int        `json:"writeTimeoutMs"`      // default: 0 (no timeout)
 	MaxBodyBytes        int64      `json:"maxBodyBytes"`        // default: 0 (no limit)
