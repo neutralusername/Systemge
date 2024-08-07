314c889158a62f50e6790bebe652cf75c7847092
fabian thomas stuck <stuck.fabian@gmail.com>
2024-08-07 09:52:47 +0200

    updated update script

--- updateSamples.sh
+++ updateSamples.sh
@@ -88,3 +88,12 @@ git add go.mod go.sum
 git commit -m "Update Systemge"
 git push
 
+cd ../SystemgeSpawnerTest/main
+export GOPROXY=direct
+go get -u github.com/neutralusername/Systemge
+cd ..
+go mod tidy
+git add go.mod go.sum
+git commit -m "Update Systemge"
+git push
+
