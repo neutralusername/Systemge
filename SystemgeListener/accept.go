7e3e8edbcf8c4d49720f40d52893d78234e4a9fc
fabian thomas stuck <stuck.fabian@gmail.com>
2024-08-15 19:26:57 +0200

    wip

---
+++ SystemgeListener/accept.go
@@ -0,0 +1,79 @@
+package SystemgeListener
+
+import (
+	"net"
+
+	"github.com/neutralusername/Systemge/Config"
+	"github.com/neutralusername/Systemge/Error"
+	"github.com/neutralusername/Systemge/Helpers"
+	"github.com/neutralusername/Systemge/Message"
+	"github.com/neutralusername/Systemge/SystemgeConnection"
+	"github.com/neutralusername/Systemge/Tcp"
+)
+
+func (listener *SystemgeListener) AcceptConnection(config *Config.SystemgeConnection) (*SystemgeConnection.SystemgeConnection, error) {
+	netConn, err := listener.tcpListener.GetListener().Accept()
+	listener.connectionId++
+	connectionId := listener.connectionId
+	listener.connectionAttempts.Add(1)
+	if err != nil {
+		listener.failedConnections.Add(1)
+		return nil, Error.New("Failed to accept connection #"+Helpers.Uint32ToString(connectionId), err)
+	}
+	ip, _, _ := net.SplitHostPort(netConn.RemoteAddr().String())
+	if infoLogger := listener.infoLogger; infoLogger != nil {
+		infoLogger.Log("Accepted connection #" + Helpers.Uint32ToString(connectionId) + " from " + ip)
+	}
+	if listener.ipRateLimiter != nil && !listener.ipRateLimiter.RegisterConnectionAttempt(ip) {
+		listener.rejectedConnections.Add(1)
+		netConn.Close()
+		return nil, Error.New("Rejected connection #"+Helpers.Uint32ToString(connectionId)+" due to rate limiting", nil)
+	}
+	if listener.tcpListener.GetBlacklist().Contains(ip) {
+		listener.rejectedConnections.Add(1)
+		netConn.Close()
+		return nil, Error.New("Rejected connection #"+Helpers.Uint32ToString(connectionId)+" due to blacklist", nil)
+	}
+	if listener.tcpListener.GetWhitelist().ElementCount() > 0 && !listener.tcpListener.GetWhitelist().Contains(ip) {
+		listener.rejectedConnections.Add(1)
+		netConn.Close()
+		return nil, Error.New("Rejected connection #"+Helpers.Uint32ToString(connectionId)+" due to whitelist", nil)
+	}
+	connection, err := listener.handshake(config, netConn)
+	if err != nil {
+		listener.rejectedConnections.Add(1)
+		netConn.Close()
+		return nil, Error.New("Rejected connection #"+Helpers.Uint32ToString(connectionId)+" due to handshake failure", err)
+	}
+	listener.acceptedConnections.Add(1)
+	if infoLogger := listener.infoLogger; infoLogger != nil {
+		infoLogger.Log("Accepted connection #" + Helpers.Uint32ToString(connectionId) + " from " + ip + " as \"" + connection.GetName() + "\"")
+	}
+	return connection, nil
+}
+
+func (listener *SystemgeListener) handshake(config *Config.SystemgeConnection, netConn net.Conn) (*SystemgeConnection.SystemgeConnection, error) {
+	messageBytes, _, err := Tcp.Receive(netConn, config.TcpReceiveTimeoutMs, config.TcpBufferBytes)
+	if err != nil {
+		return nil, Error.New("Failed to receive \""+Message.TOPIC_NAME+"\" message", err)
+	}
+	message, err := Message.Deserialize(messageBytes, "")
+	if err != nil {
+		return nil, Error.New("Failed to deserialize \""+Message.TOPIC_NAME+"\" message", err)
+	}
+	if message.GetTopic() != Message.TOPIC_NAME {
+		return nil, Error.New("Received message with unexpected topic \""+message.GetTopic()+"\" instead of \""+Message.TOPIC_NAME+"\"", nil)
+	}
+	if len(message.GetPayload()) > int(listener.config.MaxClientNameLength) {
+		return nil, Error.New("Received client name \""+message.GetPayload()+"\" exceeds maximum size of "+Helpers.Uint64ToString(listener.config.MaxClientNameLength), nil)
+	}
+	if message.GetPayload() == "" {
+		return nil, Error.New("Received empty payload in \""+Message.TOPIC_NAME+"\" message", nil)
+	}
+	clientConnectionName := message.GetPayload()
+	_, err = Tcp.Send(netConn, Message.NewAsync(Message.TOPIC_NAME, listener.config.Name).Serialize(), config.TcpSendTimeoutMs)
+	if err != nil {
+		return nil, Error.New("Failed to send \""+Message.TOPIC_NAME+"\" message", err)
+	}
+	return SystemgeConnection.New(config, netConn, clientConnectionName), nil
+}
