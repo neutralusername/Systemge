582c7574822173bd78abbb2b5d6a091481bac98a
fabian thomas stuck <stuck.fabian@gmail.com>
2024-08-21 23:05:02 +0200

    command line interface

---
+++ CLI/commandLine.go
@@ -0,0 +1,30 @@
+package CLI
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/neutralusername/Systemge/Dashboard"
+)
+
+func Start(commandHandlers Dashboard.CommandHandlers) {
+	println("Welcome to the Systemge command line interface")
+	for {
+		print("Enter a command: ")
+		var command string
+		_, _ = fmt.Scanln(&command)
+		if command == "exit" {
+			break
+		}
+		segments := strings.Split(command, " ")
+		if len(segments) == 0 {
+			continue
+		}
+		commandHandler, ok := commandHandlers[segments[0]]
+		if !ok {
+			println("Command not found")
+			continue
+		}
+		commandHandler(segments[1:])
+	}
+}
