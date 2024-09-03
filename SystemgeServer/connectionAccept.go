ccb66472f491ff9dceb9f99215913d033050d000
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-03 15:32:57 +0200

    wip

--- SystemgeServer/connectionAccept.go
+++ SystemgeServer/connectionAccept.go
@@ -58,6 +58,7 @@ func (server *SystemgeServer) handleConnections() {
 }
 
 func (server *SystemgeServer) acceptNextConnection() (SystemgeConnection.SystemgeConnection, error) {
+	// theres an edge case in the line below where the listener can be null which can cause a panic
 	connection, err := server.listener.AcceptConnection(server.GetName(), server.config.ConnectionConfig)
 	if err != nil {
 		return nil, Error.New("failed to accept connection", err)
