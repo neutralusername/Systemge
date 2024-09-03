fab2b5a471d94880c6084a7e0ef73b08b963c54f
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-03 13:32:25 +0200

    wip

--- ChannelListener/listener.go
+++ ChannelListener/listener.go
@@ -1,3 +1,4 @@
 package ChannelListener
 
 // todo: implement alternative to tcp communication for systems within a single process using go-channels
+// should be more performant because no (de-)serialization and os-func-calls are needed
