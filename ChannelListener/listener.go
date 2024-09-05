808b6564387da6e6e4d578e043056b2ddc7ffc4e
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-05 14:21:48 +0200

    todo

--- ChannelListener/listener.go
+++
@@ -1,4 +0,0 @@
-package ChannelListener
-
-// todo: implement alternative to tcp communication for systems within a single process using go-channels
-// should be more performant because no (de-)serialization and os-func-calls are needed
