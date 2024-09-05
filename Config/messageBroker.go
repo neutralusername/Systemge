6a68e77e0daf2835e58c8007da78e10bd2865c11
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-05 10:56:43 +0200

    fixed config descriptions

--- Config/messageBroker.go
+++ Config/messageBroker.go
@@ -4,15 +4,15 @@ import "encoding/json"
 
 type MessageBrokerServer struct {
 	SystemgeServerConfig  *SystemgeServer  `json:"systemgeServerConfig"`  // *required*
-	DashboardClientConfig *DashboardClient `json:"dashboardClientConfig"` // *required*
+	DashboardClientConfig *DashboardClient `json:"dashboardClientConfig"` // *optional* (it is a valid choice to leave this nil and initialize your own DashboardClient)
 
-	AsyncTopics []string `json:"asyncTopics"` // *required*
-	SyncTopics  []string `json:"syncTopics"`  // *required*
+	AsyncTopics []string `json:"asyncTopics"`
+	SyncTopics  []string `json:"syncTopics"`
 
-	InfoLoggerPath    string  `json:"infoLoggerPath"`    // *required*
-	WarningLoggerPath string  `json:"warningLoggerPath"` // *required*
-	ErrorLoggerPath   string  `json:"errorLoggerPath"`   // *required*
-	MailerConfig      *Mailer `json:"mailerConfig"`      // *required*
+	InfoLoggerPath    string  `json:"infoLoggerPath"`    // *optional*
+	WarningLoggerPath string  `json:"warningLoggerPath"` // *optional*
+	ErrorLoggerPath   string  `json:"errorLoggerPath"`   // *optional*
+	MailerConfig      *Mailer `json:"mailerConfig"`      // *optional*
 }
 
 func UnmarshalMessageBrokerServer(data string) *MessageBrokerServer {
@@ -36,7 +36,7 @@ type MessageBrokerClient struct {
 	ErrorLoggerPath   string  `json:"errorLoggerPath"`   // *optional*
 	MailerConfig      *Mailer `json:"mailerConfig"`      // *optional*
 
-	MaxServerNameLength int `json:"maxServerNameLength"` // *optional*
+	MaxServerNameLength int `json:"maxServerNameLength"` // default: 0 == no limit
 
 	AsyncTopics []string `json:"asyncTopics"`
 	SyncTopics  []string `json:"syncTopics"`
@@ -50,15 +50,15 @@ func UnmarshalMessageBrokerClient(data string) *MessageBrokerClient {
 
 type MessageBrokerResolver struct {
 	SystemgeServerConfig  *SystemgeServer  `json:"systemgeServerConfig"`  // *required*
-	DashboardClientConfig *DashboardClient `json:"dashboardClientConfig"` // *required*
+	DashboardClientConfig *DashboardClient `json:"dashboardClientConfig"` // *optional* (it is a valid choice to leave this nil and initialize your own DashboardClient)
 
-	InfoLoggerPath    string  `json:"infoLoggerPath"`    // *required*
-	WarningLoggerPath string  `json:"warningLoggerPath"` // *required*
-	ErrorLoggerPath   string  `json:"errorLoggerPath"`   // *required*
-	MailerConfig      *Mailer `json:"mailerConfig"`      // *required*
+	InfoLoggerPath    string  `json:"infoLoggerPath"`    // *optional*
+	WarningLoggerPath string  `json:"warningLoggerPath"` // *optional*
+	ErrorLoggerPath   string  `json:"errorLoggerPath"`   // *optional*
+	MailerConfig      *Mailer `json:"mailerConfig"`      // *optional*
 
-	MaxClientNameLength int `json:"maxClientNameLength"` // *required*
+	MaxClientNameLength int `json:"maxClientNameLength"` // default: 0 == no limit
 
-	AsyncTopicClientConfigs map[string]*TcpClient `json:"asyncTopicClientConfigs"` // *required*
-	SyncTopicClientConfigs  map[string]*TcpClient `json:"syncTopicClientConfigs"`  // *required*
+	AsyncTopicClientConfigs map[string]*TcpClient `json:"asyncTopicClientConfigs"`
+	SyncTopicClientConfigs  map[string]*TcpClient `json:"syncTopicClientConfigs"`
 }
