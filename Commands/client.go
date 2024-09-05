35f331713aa9660966e6f04f9f749ffc5dde7e95
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-05 17:30:53 +0200

    refactoring

--- Commands/client.go
+++ Commands/client.go
@@ -8,15 +8,17 @@ import (
 	"github.com/neutralusername/Systemge/TcpConnection"
 )
 
+type commandStruct struct {
+	Command string   `json:"command"`
+	Args    []string `json:"args"`
+}
+
 func NewCommandClient(name string, config *Config.CommandClient, command string, args ...string) (string, error) {
 	connection, err := TcpConnection.EstablishConnection(config.TcpConnectionConfig, config.TcpClientConfig, name, int(config.MaxServerNameLength))
 	if err != nil {
 		return "", Error.New("Failed to establish connection", err)
 	}
-	commandStruct := struct {
-		Command string   `json:"command"`
-		Args    []string `json:"args"`
-	}{
+	commandStruct := commandStruct{
 		Command: command,
 		Args:    args,
 	}
