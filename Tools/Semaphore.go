e08068058af9a61c5c759ed026aa172c72b3ad71
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-05 19:49:38 +0200

    simple semaphore with max and initial val

---
+++ Tools/Semaphore.go
@@ -0,0 +1,37 @@
+package Tools
+
+type Semaphore struct {
+	channel chan struct{}
+}
+
+func NewSemaphore(maxValue int, initialValue int) *Semaphore {
+	channel := make(chan struct{}, maxValue)
+	for i := 0; i < initialValue; i++ {
+		channel <- struct{}{}
+	}
+	return &Semaphore{
+		channel: channel,
+	}
+}
+
+func (semaphore *Semaphore) AcquireBlocking() {
+	<-semaphore.channel
+}
+
+func (semaphore *Semaphore) TryAcquire() bool {
+	select {
+	case <-semaphore.channel:
+		return true
+	default:
+		return false
+	}
+}
+
+func (semaphore *Semaphore) Release() bool {
+	select {
+	case semaphore.channel <- struct{}{}:
+		return true
+	default:
+		return false
+	}
+}
