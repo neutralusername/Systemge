1baa8e748d5e8784e22429dd4a6092b4ebce159a
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-08 19:16:40 +0200

    client side "scheduling"

--- Dashboard/frontend/commands.js
+++ Dashboard/frontend/commands.js
@@ -2,6 +2,7 @@ export class commands extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			intervals: {},
 		}
 		
 	}
@@ -24,7 +25,7 @@ export class commands extends React.Component {
 							this.props.WS_CONNECTION.send(this.props.constructMessage("command", JSON.stringify({
 								name: this.props.name,
 								command: command,
-								args: document.getElementById(command).value !== "" ? document.getElementById(command).value.split(" ") : [],
+								args: document.getElementById(command+"args").value !== "" ? document.getElementById(command+"args").value.split(" ") : [],
 							})));
 						},
 					},
@@ -33,10 +34,57 @@ export class commands extends React.Component {
 				React.createElement(
 					"input", {
 						type: "text",
-						id: command,
-						name: command,
+						id: command+"args",
+						name: command+"args",
 						placeholder: "args",
 					},
+				),
+				React.createElement(
+					"input", {
+						type: "text",
+						id: command+"interval",
+						name: command+"interval",
+						placeholder: "interval ms",
+						style: {
+							width: "70px",
+							marginLeft: "10px"
+						},
+					},
+				),
+				this.state.intervals[command] ? React.createElement(
+					"button", {
+						onClick: () => {
+							if (this.state.intervals[command])
+								clearInterval(this.state.intervals[command]);
+								this.setState({
+									intervals: {
+										...this.state.intervals,
+										[command]: false,
+									},
+								});
+						},
+					},
+					"Stop",
+				) : React.createElement(
+					"button", {
+						onClick: () => {
+							if (document.getElementById(command+"interval").value !== "" && !isNaN(document.getElementById(command+"interval").value)) {
+								this.setState({
+									intervals: {
+										...this.state.intervals,
+										[command]: setInterval(() => {
+											this.props.WS_CONNECTION.send(this.props.constructMessage("command", JSON.stringify({
+												name: this.props.name,
+												command: command,
+												args: document.getElementById(command+"args").value !== "" ? document.getElementById(command+"args").value.split(" ") : [],
+											})));
+										}, parseInt(document.getElementById(command+"interval").value)),
+									},
+								});
+							}
+						},
+					},
+					"Start",
 				),
 			);
 		});
