e35d691294583b9b25246b56ddefb78121196ec4
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-10 15:36:39 +0200

    wip

--- DashboardHelpers/client.go
+++ DashboardHelpers/client.go
@@ -1,12 +1,5 @@
 package DashboardHelpers
 
-import "encoding/json"
-
-type Introduction struct {
-	ClientStruct interface{} `json:"client"`
-	ClientType   int         `json:"clientType"`
-}
-
 const (
 	CLIENT_COMMAND = iota
 	CLIENT_CUSTOM_SERVICE
@@ -27,66 +20,5 @@ func HasStatus(client interface{}) bool {
 		return true
 	default:
 		return false
-	}
-}
-
-func NewIntroduction(client interface{}, clientType int) *Introduction {
-	switch clientType {
-	case CLIENT_COMMAND:
-		if _, ok := client.(*CommandClient); !ok {
-			panic("Invalid client type")
-		}
-	case CLIENT_CUSTOM_SERVICE:
-		if _, ok := client.(*CustomServiceClient); !ok {
-			panic("Invalid client type")
-		}
-	default:
-		panic("Unknown client type")
-	}
-	return &Introduction{
-		ClientStruct: client,
-		ClientType:   clientType,
-	}
-}
-
-func (introduction *Introduction) Marshal() []byte {
-	marshalClient := Introduction{}
-	marshalClient.ClientType = introduction.ClientType
-	switch introduction.ClientType {
-	case CLIENT_COMMAND:
-		marshalClient.ClientStruct = introduction.ClientStruct.(*CommandClient).Marshal()
-	case CLIENT_CUSTOM_SERVICE:
-		marshalClient.ClientStruct = introduction.ClientStruct.(*CustomServiceClient).Marshal()
-	default:
-		panic("Unknown client type")
-	}
-	bytes, err := json.Marshal(marshalClient)
-	if err != nil {
-		panic(err)
-	}
-	return bytes
-}
-
-func UnmarshalClient(data []byte) (*Introduction, error) {
-	var client Introduction
-	err := json.Unmarshal(data, &client)
-	if err != nil {
-		return nil, err
-	}
-	switch client.ClientType {
-	case CLIENT_COMMAND:
-		commandClient, err := UnmarshalCommandClient(client.ClientStruct.([]byte))
-		if err != nil {
-			return nil, err
-		}
-		return NewIntroduction(commandClient, CLIENT_COMMAND), nil
-	case CLIENT_CUSTOM_SERVICE:
-		customServiceClient, err := UnmarshalCustomClient(client.ClientStruct.([]byte))
-		if err != nil {
-			return nil, err
-		}
-		return NewIntroduction(customServiceClient, CLIENT_CUSTOM_SERVICE), nil
-	default:
-		return nil, nil
 	}
 }
---
+++ DashboardHelpers/introduction.go
@@ -0,0 +1,69 @@
+package DashboardHelpers
+
+import "encoding/json"
+
+type Introduction struct {
+	ClientStruct interface{} `json:"client"`
+	ClientType   int         `json:"clientType"`
+}
+
+func NewIntroduction(client interface{}, clientType int) *Introduction {
+	switch clientType {
+	case CLIENT_COMMAND:
+		if _, ok := client.(*CommandClient); !ok {
+			panic("Invalid client type")
+		}
+	case CLIENT_CUSTOM_SERVICE:
+		if _, ok := client.(*CustomServiceClient); !ok {
+			panic("Invalid client type")
+		}
+	default:
+		panic("Unknown client type")
+	}
+	return &Introduction{
+		ClientStruct: client,
+		ClientType:   clientType,
+	}
+}
+
+func (introduction *Introduction) Marshal() []byte {
+	marshalClient := Introduction{}
+	marshalClient.ClientType = introduction.ClientType
+	switch introduction.ClientType {
+	case CLIENT_COMMAND:
+		marshalClient.ClientStruct = introduction.ClientStruct.(*CommandClient).Marshal()
+	case CLIENT_CUSTOM_SERVICE:
+		marshalClient.ClientStruct = introduction.ClientStruct.(*CustomServiceClient).Marshal()
+	default:
+		panic("Unknown client type")
+	}
+	bytes, err := json.Marshal(marshalClient)
+	if err != nil {
+		panic(err)
+	}
+	return bytes
+}
+
+func UnmarshalIntroduction(data []byte) (interface{}, error) {
+	var client Introduction
+	err := json.Unmarshal(data, &client)
+	if err != nil {
+		return nil, err
+	}
+	switch client.ClientType {
+	case CLIENT_COMMAND:
+		commandClient, err := UnmarshalCommandClient(client.ClientStruct.([]byte))
+		if err != nil {
+			return nil, err
+		}
+		return commandClient, nil
+	case CLIENT_CUSTOM_SERVICE:
+		customServiceClient, err := UnmarshalCustomClient(client.ClientStruct.([]byte))
+		if err != nil {
+			return nil, err
+		}
+		return customServiceClient, nil
+	default:
+		return nil, nil
+	}
+}
