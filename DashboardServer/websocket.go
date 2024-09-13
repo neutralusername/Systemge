d55899d96a7c29f0ec5c80ab08afcae707a94cae
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-13 09:31:04 +0200

    wip

--- DashboardServer/websocket.go
+++ DashboardServer/websocket.go
@@ -177,7 +177,7 @@ func (server *Server) changeWebsocketClientLocation(websocketClient *WebsocketSe
 func (server *Server) getDashboardData() map[string]interface{} {
 	dashboardData := map[string]interface{}{}
 	clientStatus := map[string]int{}
-	dashboardData["clientsStatus"] = clientStatus
+	dashboardData["clientStatus"] = clientStatus
 	for _, connectedClient := range server.connectedClients {
 		clientStatus[connectedClient.connection.GetName()] = DashboardHelpers.GetStatus(connectedClient.client)
 	}
