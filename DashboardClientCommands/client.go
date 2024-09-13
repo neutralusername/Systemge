30ed6d3c727bf8c1abda2253d9df4320f4edff90
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-13 08:10:59 +0200

    wip

--- DashboardClientCommands/client.go
+++ DashboardClientCommands/client.go
@@ -1,32 +1,15 @@
 package DashboardClientCommands
 
 import (
-	"sync"
-
 	"github.com/neutralusername/Systemge/Commands"
 	"github.com/neutralusername/Systemge/Config"
+	"github.com/neutralusername/Systemge/DashboardClient"
 	"github.com/neutralusername/Systemge/DashboardHelpers"
-	"github.com/neutralusername/Systemge/Error"
 	"github.com/neutralusername/Systemge/Message"
-	"github.com/neutralusername/Systemge/Status"
 	"github.com/neutralusername/Systemge/SystemgeConnection"
-	"github.com/neutralusername/Systemge/TcpSystemgeConnection"
 )
 
-type Client struct {
-	name               string
-	config             *Config.DashboardClient
-	systemgeConnection SystemgeConnection.SystemgeConnection
-
-	messageHandler *SystemgeConnection.TopicExclusiveMessageHandler
-
-	commands Commands.Handlers
-
-	status int
-	mutex  sync.Mutex
-}
-
-func New(name string, config *Config.DashboardClient, commands Commands.Handlers) *Client {
+func New(name string, config *Config.DashboardClient, commands Commands.Handlers) *DashboardClient.Client {
 	if config == nil {
 		panic("config is nil")
 	}
@@ -39,87 +22,30 @@ func New(name string, config *Config.DashboardClient, commands Commands.Handlers
 	if config.TcpClientConfig == nil {
 		panic("config.EndpointConfig is nil")
 	}
-	app := &Client{
-		name:     name,
-		config:   config,
-		commands: commands,
-	}
-	return app
-}
-
-func (app *Client) Start() error {
-	app.mutex.Lock()
-	defer app.mutex.Unlock()
-	if app.status == Status.STARTED {
-		return Error.New("Already started", nil)
-	}
-	connection, err := TcpSystemgeConnection.EstablishConnection(app.config.TcpSystemgeConnectionConfig, app.config.TcpClientConfig, app.name, app.config.MaxServerNameLength)
-	if err != nil {
-		return Error.New("Failed to establish connection", err)
-	}
-	app.systemgeConnection = connection
-
-	message, err := connection.GetNextMessage()
-	if err != nil {
-		connection.Close()
-		return Error.New("Failed to get introduction message", err)
-	}
-	if message.GetTopic() != Message.TOPIC_INTRODUCTION {
-		connection.Close()
-		return Error.New("Expected introduction message", nil)
-	}
-	response, err := app.introductionHandler()
-	if err != nil {
-		connection.Close()
-		return Error.New("Failed to handle introduction message", err)
-	}
-	err = connection.SyncResponse(message, true, response)
-	if err != nil {
-		connection.Close()
-		return Error.New("Failed to send introduction response", err)
-	}
-
-	app.messageHandler = SystemgeConnection.NewTopicExclusiveMessageHandler(
-		nil,
-		SystemgeConnection.SyncMessageHandlers{
-			Message.TOPIC_EXECUTE_COMMAND: app.executeCommandHandler,
-		},
-		nil, nil,
-		100,
-	)
-	app.systemgeConnection.StartProcessingLoopSequentially(app.messageHandler)
-	app.status = Status.STARTED
-	return nil
-}
-
-func (app *Client) Stop() error {
-	app.mutex.Lock()
-	defer app.mutex.Unlock()
-	if app.status == Status.STOPPED {
-		return Error.New("Already stopped", nil)
-	}
-	app.systemgeConnection.StopProcessingLoop()
-	app.messageHandler.Close()
-	app.messageHandler = nil
-	app.systemgeConnection.Close()
-	app.systemgeConnection = nil
-	app.status = Status.STOPPED
-	return nil
-}
-
-func (app *Client) introductionHandler() (string, error) {
-	return string(DashboardHelpers.NewIntroduction(
-		DashboardHelpers.NewCommandClient(
-			app.name,
-			app.commands.GetKeys(),
+	return DashboardClient.New(
+		name,
+		config,
+		SystemgeConnection.NewTopicExclusiveMessageHandler(
+			nil,
+			SystemgeConnection.SyncMessageHandlers{
+				Message.TOPIC_EXECUTE_COMMAND: func(connection SystemgeConnection.SystemgeConnection, message *Message.Message) (string, error) {
+					command, err := DashboardHelpers.UnmarshalCommand(message.GetPayload())
+					if err != nil {
+						return "", err
+					}
+					return commands.Execute(command.Command, command.Args)
+				},
+			},
+			nil, nil,
+			1000,
 		),
-	).Marshal()), nil
-}
-
-func (app *Client) executeCommandHandler(connection SystemgeConnection.SystemgeConnection, message *Message.Message) (string, error) {
-	command, err := DashboardHelpers.UnmarshalCommand(message.GetPayload())
-	if err != nil {
-		return "", err
-	}
-	return app.commands.Execute(command.Command, command.Args)
+		func() (string, error) {
+			return string(DashboardHelpers.NewIntroduction(
+				DashboardHelpers.NewCommandClient(
+					name,
+					commands.GetKeys(),
+				),
+			).Marshal()), nil
+		},
+	)
 }
