57315ee84f1c9070376a07a1a86d5130d90239b3
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-13 08:20:14 +0200

    wip

---
+++ DashboardClientCustomService/customServiceStruct.go
@@ -0,0 +1,24 @@
+package DashboardClientCustomService
+
+type customServiceStruct struct {
+	startFunc      func() error
+	stopFunc       func() error
+	getStatusFunc  func() int
+	getMetricsFunc func() map[string]uint64
+}
+
+func (customService *customServiceStruct) Start() error {
+	return customService.startFunc()
+}
+
+func (customService *customServiceStruct) Stop() error {
+	return customService.stopFunc()
+}
+
+func (customService *customServiceStruct) GetStatus() int {
+	return customService.getStatusFunc()
+}
+
+func (customService *customServiceStruct) GetMetrics() map[string]uint64 {
+	return customService.getMetricsFunc()
+}
