0507b6006088f9faefb59ac9bb9f379f898fe26e
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-14 07:40:15 +0200

    wip

--- DashboardServer/updateRoutine.go
+++ DashboardServer/updateRoutine.go
@@ -9,7 +9,7 @@ import (
 func (server *Server) updateRoutine() {
 	defer server.waitGroup.Done()
 	for server.status == Status.STARTED {
-		// update on all connected clients their statuses that may change over time and add the newest metrics to the cache/check entries in cache (also on dashboard)
+		// update on all connected clients their statuses that may change over time and add the newest metrics to the cache/check number of entries in cache (also on dashboard)
 		time.Sleep(time.Duration(server.config.UpdateIntervalMs) * time.Millisecond)
 	}
 }
