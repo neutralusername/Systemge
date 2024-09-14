ad07c028f9069996c6f6ca4fbef601624bdb0b9d
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-14 12:09:33 +0200

    wip

--- DashboardServer/updateRoutine.go
+++ DashboardServer/updateRoutine.go
@@ -16,16 +16,43 @@ func (server *Server) updateRoutine() {
 		for _, connectedClient := range server.connectedClients {
 			switch connectedClient.page.Type {
 			case DashboardHelpers.CLIENT_TYPE_COMMAND:
-				server.updateConnectedClientMetrics(connectedClient)
+				if err := server.updateConnectedClientMetrics(connectedClient); err != nil {
+					if server.errorLogger != nil {
+						server.errorLogger.Log(Error.New("Failed to update metrics for \""+connectedClient.connection.GetName()+"\" in updateRoutine", err).Error())
+					}
+				}
 			case DashboardHelpers.CLIENT_TYPE_CUSTOMSERVICE:
-				server.updateConnectedClientStatus(connectedClient)
-				server.updateConnectedClientMetrics(connectedClient)
-
+				if err := server.updateConnectedClientStatus(connectedClient); err != nil {
+					if server.errorLogger != nil {
+						server.errorLogger.Log(Error.New("Failed to update status for \""+connectedClient.connection.GetName()+"\" in updateRoutine", err).Error())
+					}
+				}
+				if err := server.updateConnectedClientMetrics(connectedClient); err != nil {
+					if server.errorLogger != nil {
+						server.errorLogger.Log(Error.New("Failed to update metrics for \""+connectedClient.connection.GetName()+"\" in updateRoutine", err).Error())
+					}
+				}
 			case DashboardHelpers.CLIENT_TYPE_SYSTEMGECONNECTION:
-				server.updateConnectedClientStatus(connectedClient)
-				server.updateConnectedClientMetrics(connectedClient)
-				server.updateConnectedClientUnprocessedMessageCount(connectedClient)
-				server.updateConnectedClientIsProcessingLoopRunning(connectedClient)
+				if err := server.updateConnectedClientStatus(connectedClient); err != nil {
+					if server.errorLogger != nil {
+						server.errorLogger.Log(Error.New("Failed to update status for \""+connectedClient.connection.GetName()+"\" in updateRoutine", err).Error())
+					}
+				}
+				if err := server.updateConnectedClientMetrics(connectedClient); err != nil {
+					if server.errorLogger != nil {
+						server.errorLogger.Log(Error.New("Failed to update metrics for \""+connectedClient.connection.GetName()+"\" in updateRoutine", err).Error())
+					}
+				}
+				if err := server.updateConnectedClientUnprocessedMessageCount(connectedClient); err != nil {
+					if server.errorLogger != nil {
+						server.errorLogger.Log(Error.New("Failed to update unprocessed message count for \""+connectedClient.connection.GetName()+"\" in updateRoutine", err).Error())
+					}
+				}
+				if err := server.updateConnectedClientIsProcessingLoopRunning(connectedClient); err != nil {
+					if server.errorLogger != nil {
+						server.errorLogger.Log(Error.New("Failed to update is processing loop running for \""+connectedClient.connection.GetName()+"\" in updateRoutine", err).Error())
+					}
+				}
 			}
 		}
 		server.updateDashboardClientMetrics()
@@ -34,79 +61,77 @@ func (server *Server) updateRoutine() {
 	}
 }
 
-func (server *Server) updateConnectedClientStatus(connectedClient *connectedClient) {
+func (server *Server) updateConnectedClientStatus(connectedClient *connectedClient) error {
 	resultPayload, err := connectedClient.executeRequest(DashboardHelpers.TOPIC_GET_STATUS, "")
 	if err != nil {
-		if server.errorLogger != nil {
-			server.errorLogger.Log(Error.New("Failed to get status from connected client in updateRoutine \""+connectedClient.connection.GetName()+"\"", err).Error())
-		}
-	} else {
-		connectedClient.page.SetCachedStatus(Helpers.StringToInt(resultPayload))
-		server.websocketServer.Multicast(
-			server.GetWebsocketClientIdsOnPage(DASHBOARD_CLIENT_NAME),
-			Message.NewAsync(
-				DashboardHelpers.TOPIC_UPDATE_PAGE_MERGE,
-				DashboardHelpers.NewPageUpdate(
-					map[string]interface{}{
-						DashboardHelpers.CLIENT_FIELD_CLIENTSTATUSES: map[string]int{
-							connectedClient.connection.GetName(): Helpers.StringToInt(resultPayload),
-						},
-					},
-					DASHBOARD_CLIENT_NAME,
-				).Marshal(),
-			),
-		)
-		server.websocketServer.Multicast(
-			server.GetWebsocketClientIdsOnPage(connectedClient.connection.GetName()),
-			Message.NewAsync(
-				DashboardHelpers.TOPIC_UPDATE_PAGE_MERGE,
-				DashboardHelpers.NewPageUpdate(
-					map[string]interface{}{
-						DashboardHelpers.CLIENT_FIELD_STATUS: Helpers.StringToInt(resultPayload),
-					},
-					connectedClient.connection.GetName(),
-				).Marshal(),
-			),
-		)
+		return Error.New("Failed to execute get status request", err)
 	}
+	connectedClient.page.SetCachedStatus(Helpers.StringToInt(resultPayload))
+	server.websocketServer.Multicast(
+		server.GetWebsocketClientIdsOnPage(DASHBOARD_CLIENT_NAME),
+		Message.NewAsync(
+			DashboardHelpers.TOPIC_UPDATE_PAGE_MERGE,
+			DashboardHelpers.NewPageUpdate(
+				map[string]interface{}{
+					DashboardHelpers.CLIENT_FIELD_CLIENTSTATUSES: map[string]int{
+						connectedClient.connection.GetName(): Helpers.StringToInt(resultPayload),
+					},
+				},
+				DASHBOARD_CLIENT_NAME,
+			).Marshal(),
+		),
+	)
+	server.websocketServer.Multicast(
+		server.GetWebsocketClientIdsOnPage(connectedClient.connection.GetName()),
+		Message.NewAsync(
+			DashboardHelpers.TOPIC_UPDATE_PAGE_MERGE,
+			DashboardHelpers.NewPageUpdate(
+				map[string]interface{}{
+					DashboardHelpers.CLIENT_FIELD_STATUS: Helpers.StringToInt(resultPayload),
+				},
+				connectedClient.connection.GetName(),
+			).Marshal(),
+		),
+	)
+	return nil
 }
 
-func (server *Server) updateConnectedClientMetrics(connectedClient *connectedClient) {
+func (server *Server) updateConnectedClientMetrics(connectedClient *connectedClient) error {
 	resultPayload, err := connectedClient.executeRequest(DashboardHelpers.TOPIC_GET_METRICS, "")
 	if err != nil {
 		if server.errorLogger != nil {
-			server.errorLogger.Log(Error.New("Failed to get metrics from connected client in updateRoutine \""+connectedClient.connection.GetName()+"\"", err).Error())
+			server.errorLogger.Log(Error.New("Failed to execute get metrics request", err).Error())
 		}
-	} else {
-		metrics := DashboardHelpers.UnmarshalMetrics(resultPayload)
-		for metricsName, metricsKeyValuePairs := range metrics {
-			for key, value := range metricsKeyValuePairs {
-				connectedClient.page.AddCachedMetricsEntry(metricsName, key, value, server.config.MaxMetricsCacheValues)
-			}
-		}
-		server.websocketServer.Multicast(
-			server.GetWebsocketClientIdsOnPage(connectedClient.connection.GetName()),
-			Message.NewAsync(
-				DashboardHelpers.TOPIC_UPDATE_PAGE_MERGE,
-				DashboardHelpers.NewPageUpdate(
-					map[string]interface{}{
-						DashboardHelpers.CLIENT_FIELD_METRICS: metrics,
-					},
-					connectedClient.connection.GetName(),
-				).Marshal(),
-			),
-		)
 	}
+	metrics := DashboardHelpers.UnmarshalMetrics(resultPayload)
+	for metricsName, metricsKeyValuePairs := range metrics {
+		for key, value := range metricsKeyValuePairs {
+			connectedClient.page.AddCachedMetricsEntry(metricsName, key, value, server.config.MaxMetricsCacheValues)
+		}
+	}
+	server.websocketServer.Multicast(
+		server.GetWebsocketClientIdsOnPage(connectedClient.connection.GetName()),
+		Message.NewAsync(
+			DashboardHelpers.TOPIC_UPDATE_PAGE_MERGE,
+			DashboardHelpers.NewPageUpdate(
+				map[string]interface{}{
+					DashboardHelpers.CLIENT_FIELD_METRICS: metrics,
+				},
+				connectedClient.connection.GetName(),
+			).Marshal(),
+		),
+	)
+	return nil
 }
 
-func (server *Server) updateConnectedClientUnprocessedMessageCount(connectedClient *connectedClient) {
+func (server *Server) updateConnectedClientUnprocessedMessageCount(connectedClient *connectedClient) error {
 
 }
 
-func (server *Server) updateConnectedClientIsProcessingLoopRunning(connectedClient *connectedClient) {
+func (server *Server) updateConnectedClientIsProcessingLoopRunning(connectedClient *connectedClient) error {
 
 }
 
-func (server *Server) updateDashboardClientMetrics() {
+func (server *Server) updateDashboardClientMetrics() error {
 
 }
