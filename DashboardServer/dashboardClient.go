1166b051337ab323f5986cb2fe143b8ca3498f4a
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-15 18:05:10 +0200

    wip

--- DashboardServer/dashboardClient.go
+++ DashboardServer/dashboardClient.go
@@ -21,6 +21,9 @@ func (server *Server) addMetricsToDashboardClient(metrics map[string]*Metrics.Me
 			server.dashboardClient.Metrics[metricsType] = []*Metrics.Metrics{}
 		}
 		server.dashboardClient.Metrics[metricsType] = append(server.dashboardClient.Metrics[metricsType], metrics)
+		if server.config.MaxMetricEntries > 0 && len(server.dashboardClient.Metrics[metricsType]) > server.config.MaxMetricEntries {
+			server.dashboardClient.Metrics[metricsType] = server.dashboardClient.Metrics[metricsType][1:]
+		}
 	}
 }
 
