4eb6c12a9c583b83f2e832fc35ab6e9dab5f7f58
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-16 17:22:00 +0200

    wip ..

--- DashboardHelpers/clientSystemgeServer.go
+++ DashboardHelpers/clientSystemgeServer.go
@@ -3,11 +3,11 @@ package DashboardHelpers
 import "encoding/json"
 
 type SystemgeServerClient struct {
-	Name     string           `json:"name"`
-	Commands map[string]bool  `json:"commands"`
-	Status   int              `json:"status"`  //periodically automatically updated by the server
-	Metrics  DashboardMetrics `json:"metrics"` //periodically automatically updated by the server
-	SystemgeConnections map[string]??? //periodically automatically updated by the server
+	Name                string                               `json:"name"`
+	Commands            map[string]bool                      `json:"commands"`
+	Status              int                                  `json:"status"`              //periodically automatically updated by the server
+	Metrics             DashboardMetrics                     `json:"metrics"`             //periodically automatically updated by the server
+	SystemgeConnections map[string]*SystemgeConnectionClient `json:"systemgeConnections"` //periodically automatically updated by the server
 }
 
 func NewSystemgeServerClient(name string, commands map[string]bool, status int, metrics DashboardMetrics) *SystemgeServerClient {
