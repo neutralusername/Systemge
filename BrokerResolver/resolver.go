d9ba08a3ceebb40b8627ec0c39aae8ed22cdee60
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-17 07:52:45 +0200

    wip

--- BrokerResolver/resolver.go
+++ BrokerResolver/resolver.go
@@ -1,16 +1,12 @@
 package BrokerResolver
 
 import (
-	"encoding/json"
 	"sync"
 	"sync/atomic"
 
-	"github.com/neutralusername/Systemge/Commands"
 	"github.com/neutralusername/Systemge/Config"
-	"github.com/neutralusername/Systemge/Error"
 	"github.com/neutralusername/Systemge/Helpers"
 	"github.com/neutralusername/Systemge/Message"
-	"github.com/neutralusername/Systemge/Status"
 	"github.com/neutralusername/Systemge/SystemgeMessageHandler"
 	"github.com/neutralusername/Systemge/SystemgeServer"
 	"github.com/neutralusername/Systemge/Tools"
@@ -122,83 +118,4 @@ func (resolver *Resolver) GetStatus() int {
 
 func (resolver *Resolver) GetName() string {
 	return resolver.name
-}
-
-func (server *Resolver) GetDefaultCommands() Commands.Handlers {
-	commands := Commands.Handlers{
-		"start": func(args []string) (string, error) {
-			err := server.Start()
-			if err != nil {
-				return "", Error.New("failed to start message broker server", err)
-			}
-			return "success", nil
-		},
-		"stop": func(args []string) (string, error) {
-			err := server.Stop()
-			if err != nil {
-				return "", Error.New("failed to stop message broker server", err)
-			}
-			return "success", nil
-		},
-		"getStatus": func(args []string) (string, error) {
-			return Status.ToString(server.GetStatus()), nil
-		},
-		"checkMetrics": func(args []string) (string, error) {
-			metrics := server.CheckMetrics()
-			json, err := json.Marshal(metrics)
-			if err != nil {
-				return "", Error.New("failed to marshal metrics to json", err)
-			}
-			return string(json), nil
-		},
-		"getMetrics": func(args []string) (string, error) {
-			metrics := server.GetMetrics()
-			json, err := json.Marshal(metrics)
-			if err != nil {
-				return "", Error.New("failed to marshal metrics to json", err)
-			}
-			return string(json), nil
-		},
-		"addAsyncResolution": func(args []string) (string, error) {
-			if len(args) != 2 {
-				return "", Error.New("expected 2 arguments", nil)
-			}
-			endpointConfig := Config.UnmarshalTcpClient(args[1])
-			if endpointConfig == nil {
-				return "", Error.New("failed unmarshalling endpointConfig", nil)
-			}
-			server.AddAsyncResolution(args[0], endpointConfig)
-			return "success", nil
-		},
-		"removeAsyncResolution": func(args []string) (string, error) {
-			if len(args) != 1 {
-				return "", Error.New("expected 1 argument", nil)
-			}
-			server.RemoveAsyncResolution(args[0])
-			return "success", nil
-		},
-		"addSyncResolution": func(args []string) (string, error) {
-			if len(args) != 2 {
-				return "", Error.New("expected 2 arguments", nil)
-			}
-			endpointConfig := Config.UnmarshalTcpClient(args[1])
-			if endpointConfig == nil {
-				return "", Error.New("failed unmarshalling endpointConfig", nil)
-			}
-			server.AddSyncResolution(args[0], endpointConfig)
-			return "success", nil
-		},
-		"removeSyncResolution": func(args []string) (string, error) {
-			if len(args) != 1 {
-				return "", Error.New("expected 1 argument", nil)
-			}
-			server.RemoveSyncResolution(args[0])
-			return "success", nil
-		},
-	}
-	systemgeServerCommands := server.systemgeServer.GetDefaultCommands()
-	for key, value := range systemgeServerCommands {
-		commands["systemgeServer_"+key] = value
-	}
-	return commands
 }
--- BrokerServer/server.go
+++ BrokerServer/server.go
@@ -1,15 +1,11 @@
 package BrokerServer
 
 import (
-	"encoding/json"
 	"sync"
 	"sync/atomic"
 
-	"github.com/neutralusername/Systemge/Commands"
 	"github.com/neutralusername/Systemge/Config"
-	"github.com/neutralusername/Systemge/Error"
 	"github.com/neutralusername/Systemge/Message"
-	"github.com/neutralusername/Systemge/Status"
 	"github.com/neutralusername/Systemge/SystemgeConnection"
 	"github.com/neutralusername/Systemge/SystemgeMessageHandler"
 	"github.com/neutralusername/Systemge/SystemgeServer"
@@ -120,63 +116,4 @@ func (server *Server) GetStatus() int {
 
 func (server *Server) GetName() string {
 	return server.name
-}
-
-func (server *Server) GetDefaultCommands() Commands.Handlers {
-	commands := Commands.Handlers{
-		"start": func(args []string) (string, error) {
-			err := server.Start()
-			if err != nil {
-				return "", Error.New("failed to start message broker server", err)
-			}
-			return "success", nil
-		},
-		"stop": func(args []string) (string, error) {
-			err := server.Stop()
-			if err != nil {
-				return "", Error.New("failed to stop message broker server", err)
-			}
-			return "success", nil
-		},
-		"getStatus": func(args []string) (string, error) {
-			return Status.ToString(server.GetStatus()), nil
-		},
-		"checkMetrics": func(args []string) (string, error) {
-			metrics := server.CheckMetrics()
-			json, err := json.Marshal(metrics)
-			if err != nil {
-				return "", Error.New("failed to marshal metrics to json", err)
-			}
-			return string(json), nil
-		},
-		"getMetrics": func(args []string) (string, error) {
-			metrics := server.GetMetrics()
-			json, err := json.Marshal(metrics)
-			if err != nil {
-				return "", Error.New("failed to marshal metrics to json", err)
-			}
-			return string(json), nil
-		},
-		"addAsyncTopics": func(args []string) (string, error) {
-			server.AddAsyncTopics(args)
-			return "success", nil
-		},
-		"removeAsyncTopics": func(args []string) (string, error) {
-			server.RemoveAsyncTopics(args)
-			return "success", nil
-		},
-		"addSyncTopics": func(args []string) (string, error) {
-			server.AddSyncTopics(args)
-			return "success", nil
-		},
-		"removeSyncTopics": func(args []string) (string, error) {
-			server.RemoveSyncTopics(args)
-			return "success", nil
-		},
-	}
-	systemgeServerCommands := server.systemgeServer.GetDefaultCommands()
-	for key, value := range systemgeServerCommands {
-		commands["systemgeServer_"+key] = value
-	}
-	return commands
 }
