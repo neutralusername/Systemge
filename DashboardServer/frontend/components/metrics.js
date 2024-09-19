dffdf020e44e81f549607a838fc953d757602cf9
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-19 14:03:30 +0200

    wip

--- DashboardServer/frontend/components/metrics.js
+++ DashboardServer/frontend/components/metrics.js
@@ -1,17 +1,100 @@
+import { 
+    multiLineGraph 
+} from "./graphs/multiLineGraph.js";
 
 // expects props:
 export class metrics extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			distinctColors : [
+                "#556b2f",
+                "#7f0000",
+                "#483d8b",
+                "#008000",
+                "#b8860b",
+                "#008b8b",
+                "#00008b",  
+                "#32cd32",
+                "#7f007f",
+                "#8fbc8f",
+                "#b03060",
+                "#ff0000",
+                "#ff8c00",
+                "#00ff00",
+                "#8a2be2",
+                "#dc143c",
+                "#00ffff",
+                "#00bfff",
+                "#0000ff",
+                "#adff2f",
+                "#da70d6",
+                "#ff00ff",
+                "#1e90ff",
+                "#f0e68c",
+                "#fa8072",
+                "#ffff54",
+                "#b0e0e6",
+                "#90ee90",
+                "#ff1493",
+                "#7b68ee",
+                "#ffb6c1",
+            ],
 		}
 	}
+
+	generateRandomDistinctColors = (strings) => {
+        let colors = [];
+        strings.forEach(str => {
+            const hash = this.generateHash(str);
+            const colorIndex = hash % this.state.distinctColors.length;
+            colors.push(this.state.distinctColors[colorIndex]);
+        });
+        return colors;
+    }
+
+	generateHash = (str) => {
+        let hash = 0;
+        for (let i = 0; i < str.length && i < 30; i++) {
+            const char = str.charCodeAt(i);
+            hash = ((hash << 5) - hash) + char;
+            hash |= 0; // Convert to 32bit integer
+        }
+        return Math.abs(hash);
+    }
+
+	getMultiLineGraph = (chartName, metrics) => {
+        // metrics = [{keyValuePairs:{key1:value1}, time:time1},...]
+        let dataSet = {}; // {key1:[value1, value2, ...], ...}
+        let timestamps = []; // [time1, time2, ...]
+        metrics.forEach((metric) => {
+            timestamps.push(new Date(metric.time).toLocaleTimeString());
+            Object.keys(metric.keyValuePairs).forEach((key) => {
+                if (dataSet[key] === undefined) {
+                    dataSet[key] = [];
+                }
+                dataSet[key].push(metric.keyValuePairs[key]);
+            });
+        })
+        return React.createElement(
+            multiLineGraph, {
+                title: chartName,
+                chartName: `${chartName}`,
+                dataLabels: Object.keys(dataSet),
+                dataSet: dataSet,
+                labels : timestamps,
+                colors : this.generateRandomDistinctColors(Object.keys(dataSet)),
+                height: "40vh",
+                width: "60vw",
+            },
+        );
+    }
 
 	render() {
         let graphs = [];
 		Object.keys(this.props.pageData.metrics).forEach((metricType) => {
 			let metrics = this.props.pageData.metrics[metricType];
-			graphs.push(this.props.getMultiLineGraph(metricType, metrics));
+			graphs.push(this.getMultiLineGraph(metricType, metrics));
 		})
 		return React.createElement(
 			"div", {
--- DashboardServer/frontend/root/root.js
+++ DashboardServer/frontend/root/root.js
@@ -2,9 +2,6 @@ import {
    configs
 } from "../configs.js";
 import { 
-    multiLineGraph 
-} from "../components/graphs/multiLineGraph.js";
-import { 
     GenerateRandomAlphaNumericString 
 } from "../helpers/randomizer.js";
 import {
@@ -46,46 +43,10 @@ export class root extends React.Component {
                 this.setState(state);
             },
             WS_CONNECTION : GetWebsocketConnection(configs.WS_PORT, configs.WS_PATTERN),
-            distinctColors : [
-                "#556b2f",
-                "#7f0000",
-                "#483d8b",
-                "#008000",
-                "#b8860b",
-                "#008b8b",
-                "#00008b",  
-                "#32cd32",
-                "#7f007f",
-                "#8fbc8f",
-                "#b03060",
-                "#ff0000",
-                "#ff8c00",
-                "#00ff00",
-                "#8a2be2",
-                "#dc143c",
-                "#00ffff",
-                "#00bfff",
-                "#0000ff",
-                "#adff2f",
-                "#da70d6",
-                "#ff00ff",
-                "#1e90ff",
-                "#f0e68c",
-                "#fa8072",
-                "#ffff54",
-                "#b0e0e6",
-                "#90ee90",
-                "#ff1493",
-                "#7b68ee",
-                "#ffb6c1",
-            ],
-            generateRandomDistinctColors: this.generateRandomDistinctColors,
             pageRequest: this.pageRequest,
-            getMultiLineGraph: this.getMultiLineGraph,
         };
         this.mergeData = this.mergeData.bind(this);
         this.setResponseMessage = this.setResponseMessage.bind(this);
-        this.generateHash = this.generateHash.bind(this);
         this.handleClose = this.handleClose.bind(this);
         this.handleOpen = this.handleOpen.bind(this);
         this.getContent = this.getContent.bind(this);
@@ -211,26 +172,6 @@ export class root extends React.Component {
         })
     }
 
-    generateHash = (str) => {
-        let hash = 0;
-        for (let i = 0; i < str.length && i < 30; i++) {
-            const char = str.charCodeAt(i);
-            hash = ((hash << 5) - hash) + char;
-            hash |= 0; // Convert to 32bit integer
-        }
-        return Math.abs(hash);
-    }
-
-    generateRandomDistinctColors = (strings) => {
-        let colors = [];
-        strings.forEach(str => {
-            const hash = this.generateHash(str);
-            const colorIndex = hash % this.state.distinctColors.length;
-            colors.push(this.state.distinctColors[colorIndex]);
-        });
-        return colors;
-    }
-
     setResponseMessage = (message) => {
         let responseId = GenerateRandomAlphaNumericString(10);
         while (this.state.responseMessages[responseId] !== undefined) {
@@ -254,33 +195,6 @@ export class root extends React.Component {
             responseMessages: responseMessages,
             responseMessageTimeouts: responseMessageTimeouts,
         });
-    }
-
-    getMultiLineGraph = (chartName, metrics) => {
-        // metrics = [{keyValuePairs:{key1:value1}, time:time1},...]
-        let dataSet = {}; // {key1:[value1, value2, ...], ...}
-        let timestamps = []; // [time1, time2, ...]
-        metrics.forEach((metric) => {
-            timestamps.push(new Date(metric.time).toLocaleTimeString());
-            Object.keys(metric.keyValuePairs).forEach((key) => {
-                if (dataSet[key] === undefined) {
-                    dataSet[key] = [];
-                }
-                dataSet[key].push(metric.keyValuePairs[key]);
-            });
-        })
-        return React.createElement(
-            multiLineGraph, {
-                title: chartName,
-                chartName: `${chartName}`,
-                dataLabels: Object.keys(dataSet),
-                dataSet: dataSet,
-                labels : timestamps,
-                colors : this.state.generateRandomDistinctColors(Object.keys(dataSet)),
-                height: "400px",
-                width: "1200px",
-            },
-        );
     }
 
     getContent() {
