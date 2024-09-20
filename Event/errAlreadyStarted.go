a99242d68229d7872fc0d2a3de5920720088cfec
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-20 20:14:15 +0200

    wip

--- Event/errAlreadyStarted.go
+++ Event/errAlreadyStarted.go
@@ -1,20 +1,7 @@
 package Event
 
-import "errors"
+type ErrAlreadyStarted string
 
-type ErrAlreadyStarted struct {
-	err error
-}
-
-func (e *ErrAlreadyStarted) Error() string {
-	return e.err.Error()
-}
-
-func NewErrAlreadyStarted(err string) *ErrAlreadyStarted {
-	if err == "" {
-		err = "already started"
-	}
-	return &ErrAlreadyStarted{
-		err: errors.New(err),
-	}
+func (e ErrAlreadyStarted) Error() Type {
+	return Type(e)
 }
