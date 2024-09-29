79c410b8b836fc06dad6a88add5520b378368570
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-29 15:59:56 +0200

    wip

--- Tools/sessionManager.go
+++ Tools/sessionManager.go
@@ -3,6 +3,9 @@ package Tools
 type SessionManager struct {
 	sessions   map[string]*Session
 	identities map[string]*Identity
+
+	onExpire func(*Session)
+	onCreate func(*Session)
 }
 
 type Session struct {
