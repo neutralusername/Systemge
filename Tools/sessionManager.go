5b6c62f8fe9bf8ad7a5e9b3c74fe9317fba1ddc6
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-29 16:06:41 +0200

    wip

--- Tools/sessionManager.go
+++ Tools/sessionManager.go
@@ -1,5 +1,9 @@
 package Tools
 
+import (
+	"github.com/neutralusername/Systemge/Constants"
+)
+
 type SessionManager struct {
 	sessions   map[string]*Session
 	identities map[string]*Identity
@@ -8,12 +12,50 @@ type SessionManager struct {
 	onCreate func(*Session)
 }
 
+type Identity struct {
+	id       string
+	sessions map[string]*Session
+}
+
 type Session struct {
 	id       string
 	identity *Identity
 }
 
-type Identity struct {
-	id       string
-	sessions map[string]*Session
+func NewSessionManager(onExpire func(*Session), onCreate func(*Session)) *SessionManager {
+	return &SessionManager{
+		sessions:   make(map[string]*Session),
+		identities: make(map[string]*Identity),
+
+		onExpire: onExpire,
+		onCreate: onCreate,
+	}
+}
+
+func (manager *SessionManager) CreateSession(identityString string) *Session {
+	identity, ok := manager.identities[identityString]
+	if !ok {
+		identity = &Identity{
+			id:       identityString,
+			sessions: make(map[string]*Session),
+		}
+
+		manager.identities[identity.id] = identity
+	}
+
+	sessionId := GenerateRandomString(Constants.SessionIdLength, ALPHA_NUMERIC)
+	for _, ok := manager.sessions[sessionId]; ok; {
+		sessionId = GenerateRandomString(Constants.SessionIdLength, ALPHA_NUMERIC)
+	}
+	session := &Session{
+		id:       sessionId,
+		identity: identity,
+	}
+
+	identity.sessions[session.id] = session
+	manager.sessions[session.id] = session
+
+	manager.onCreate(session)
+
+	return session
 }
