c7fd5847638dc7dffe59852fec750c245886c20e
fabian thomas stuck <stuck.fabian@gmail.com>
2024-09-29 08:07:18 +0200

    wip

--- TcpSystemgeConnect/connectAttempts.go
+++ TcpSystemgeConnect/connectAttempts.go
@@ -2,6 +2,7 @@ package TcpSystemgeConnect
 
 import (
 	"errors"
+	"sync"
 	"time"
 
 	"github.com/neutralusername/Systemge/Config"
@@ -11,12 +12,12 @@ import (
 
 type ConnectionAttempt struct {
 	config             *Config.SystemgeConnectionAttempt
-	attempts           uint32
-	isAborted          bool
-	ongoing            chan bool
-	err                error
-	systemgeConnection SystemgeConnection.SystemgeConnection
 	eventHandler       Event.Handler
+	systemgeConnection SystemgeConnection.SystemgeConnection
+
+	attempts   uint32
+	ongoing    chan bool
+	abortMutex sync.Mutex
 }
 
 func EstablishConnectionAttempts(name string, config *Config.SystemgeConnectionAttempt, eventHandler Event.Handler) (*ConnectionAttempt, error) {
@@ -32,13 +33,47 @@ func EstablishConnectionAttempts(name string, config *Config.SystemgeConnectionA
 	go connectionAttempts.connectionAttempts(name)
 	return connectionAttempts, nil
 }
+func (connectionAttempt *ConnectionAttempt) connectionAttempts(name string) {
+	for {
+		select {
+		case <-connectionAttempt.ongoing:
+			return
+		default:
+		}
+		if connectionAttempt.config.MaxConnectionAttempts > 0 && connectionAttempt.attempts >= connectionAttempt.config.MaxConnectionAttempts {
+			connectionAttempt.AbortAttempts()
+			return
+		}
+		connectionAttempt.attempts++
+		connection, err := EstablishConnection(connectionAttempt.config.TcpSystemgeConnectionConfig, connectionAttempt.config.TcpClientConfig, name, connectionAttempt.config.MaxServerNameLength, connectionAttempt.eventHandler)
+		if err != nil {
+			select {
+			case <-time.After(time.Duration(connectionAttempt.config.RetryIntervalMs) * time.Millisecond):
+			case <-connectionAttempt.ongoing:
+				return
+			}
+			continue
+		}
+		select {
+		case <-connectionAttempt.ongoing:
+			connection.Close()
+			return
+		default:
+		}
+		connectionAttempt.systemgeConnection = connection
+		connectionAttempt.AbortAttempts()
+		return
+	}
+}
 
 func (connectionAttempt *ConnectionAttempt) AbortAttempts() error {
+	connectionAttempt.abortMutex.Lock()
+	defer connectionAttempt.abortMutex.Unlock()
 	select {
 	case <-connectionAttempt.ongoing:
 		return errors.New("connection attempt has already ended")
 	default:
-		connectionAttempt.isAborted = true
+		close(connectionAttempt.ongoing)
 		return nil
 	}
 }
@@ -64,37 +99,7 @@ func (connectionAttempt *ConnectionAttempt) IsOngoing() bool {
 	}
 }
 
-func (connectionAttempt *ConnectionAttempt) GetResultBlocking() (SystemgeConnection.SystemgeConnection, error) {
+func (connectionAttempt *ConnectionAttempt) GetResultBlocking() SystemgeConnection.SystemgeConnection {
 	<-connectionAttempt.ongoing
-	return connectionAttempt.systemgeConnection, connectionAttempt.err
-}
-
-func (connectionAttempt *ConnectionAttempt) connectionAttempts(name string) {
-	for {
-		if connectionAttempt.isAborted {
-			connectionAttempt.err = errors.New("connection attempt aborted before establishing connection")
-			close(connectionAttempt.ongoing)
-			return
-		}
-		if connectionAttempt.config.MaxConnectionAttempts > 0 && connectionAttempt.attempts >= connectionAttempt.config.MaxConnectionAttempts {
-			connectionAttempt.err = errors.New("max connection attempts reached")
-			close(connectionAttempt.ongoing)
-			return
-		}
-		connectionAttempt.attempts++
-		connection, err := EstablishConnection(connectionAttempt.config.TcpSystemgeConnectionConfig, connectionAttempt.config.TcpClientConfig, name, connectionAttempt.config.MaxServerNameLength, connectionAttempt.eventHandler)
-		if err != nil {
-			time.Sleep(time.Duration(connectionAttempt.config.RetryIntervalMs) * time.Millisecond)
-			continue
-		}
-		if connectionAttempt.isAborted {
-			connection.Close()
-			connectionAttempt.err = errors.New("connection attempt aborted after establishing connection")
-			close(connectionAttempt.ongoing)
-			return
-		}
-		connectionAttempt.systemgeConnection = connection
-		close(connectionAttempt.ongoing)
-		return
-	}
+	return connectionAttempt.systemgeConnection
 }
