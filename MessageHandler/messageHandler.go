a8e5a70615ef4204a3b9d18afe72abca5e681fa4
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-01 17:04:38 +0200

    wip

--- MessageHandler/messageHandler.go
+++ MessageHandler/messageHandler.go
@@ -1,3 +1,4 @@
 package MessageHandler
 
 // implement this messageHandler that can be used for websocket and systemge connections and implements the 3 existing types of messageHandlers
+// include event Handlers to wrap the handlings in events
