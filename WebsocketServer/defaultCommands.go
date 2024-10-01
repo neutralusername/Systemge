a620d5fa4e75fe13ec9f1c877a68ab44cedb5fcc
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-01 14:57:05 +0200

    wip

--- WebsocketServer/defaultCommands.go
+++ WebsocketServer/defaultCommands.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 
 	"github.com/neutralusername/Systemge/Commands"
-	"github.com/neutralusername/Systemge/Helpers"
 	"github.com/neutralusername/Systemge/Message"
 	"github.com/neutralusername/Systemge/Status"
 )
@@ -99,125 +98,6 @@ func (server *WebsocketServer) GetDefaultCommands() Commands.Handlers {
 			return "", err
 		}
 		return "success", nil
-	}
-	commands["addWebsocketConnectionssToGroup_transactional"] = func(args []string) (string, error) {
-		if len(args) < 2 {
-			return "", errors.New("invalid number of arguments")
-		}
-		group := args[0]
-		ids := args[1:]
-		err := server.AddWebsocketConnectionsToGroup_transactional(group, ids...)
-		if err != nil {
-			return "", err
-		}
-		return "success", nil
-	}
-	commands["addWebsocketConnectionsToGroup_bestEffort"] = func(args []string) (string, error) {
-		if len(args) < 2 {
-			return "", errors.New("invalid number of arguments")
-		}
-		group := args[0]
-		ids := args[1:]
-		err := server.AddWebsocketConnectionsToGroup_bestEffort(group, ids...)
-		if err != nil {
-			return "", err
-		}
-		return "success", nil
-	}
-	commands["removeWebsocketConnectionsFromGroup_transactional"] = func(args []string) (string, error) {
-		if len(args) < 2 {
-			return "", errors.New("invalid number of arguments")
-		}
-		group := args[0]
-		ids := args[1:]
-		err := server.RemoveWebsocketConnectionsFromGroup_transactional(group, ids...)
-		if err != nil {
-			return "", err
-		}
-		return "success", nil
-	}
-	commands["removeWebsocketConnectionsFromGroup_bestEffort"] = func(args []string) (string, error) {
-		if len(args) < 2 {
-			return "", errors.New("invalid number of arguments")
-		}
-		group := args[0]
-		ids := args[1:]
-		err := server.RemoveWebsocketConnectionsFromGroup_bestEffort(group, ids...)
-		if err != nil {
-			return "", err
-		}
-		return "success", nil
-	}
-	commands["getGroupWebsocketConnectionIds"] = func(args []string) (string, error) {
-		if len(args) != 1 {
-			return "", errors.New("invalid number of arguments")
-		}
-		group := args[0]
-		websocketConnectionIds, err := server.GetGroupWebsocketConnectionIds(group)
-		if err != nil {
-			return "", err
-		}
-		json, err := json.Marshal(websocketConnectionIds)
-		if err != nil {
-			return "", err
-		}
-		return string(json), nil
-	}
-	commands["getWebsocketConnectionGroupIds"] = func(args []string) (string, error) {
-		if len(args) != 1 {
-			return "", errors.New("invalid number of arguments")
-		}
-		id := args[0]
-		groups, err := server.GetWebsocketConnectionGroupIds(id)
-		if err != nil {
-			return "", err
-		}
-		json, err := json.Marshal(groups)
-		if err != nil {
-			return "", err
-		}
-		return string(json), nil
-	}
-	commands["getGroupCount"] = func(args []string) (string, error) {
-		groupCount, err := server.GetGroupCount()
-		if err != nil {
-			return "", err
-		}
-		return Helpers.IntToString(groupCount), nil
-	}
-	commands["getGroupIds"] = func(args []string) (string, error) {
-		groups, err := server.GetGroupIds()
-		if err != nil {
-			return "", err
-		}
-		json, err := json.Marshal(groups)
-		if err != nil {
-			return "", err
-		}
-		return string(json), nil
-	}
-	commands["isWebsocketConnectionInGroup"] = func(args []string) (string, error) {
-		if len(args) != 2 {
-			return "", errors.New("invalid number of arguments")
-		}
-		id := args[0]
-		group := args[1]
-		inGroup, err := server.IsWebsocketConnectionInGroup(id, group)
-		if err != nil {
-			return "", err
-		}
-		return Helpers.BoolToString(inGroup), nil
-	}
-	commands["getWebsocketConnectionGroupCount"] = func(args []string) (string, error) {
-		if len(args) != 1 {
-			return "", errors.New("invalid number of arguments")
-		}
-		id := args[0]
-		websocketConnectionGroupCount, err := server.GetWebsocketConnectionGroupCount(id)
-		if err != nil {
-			return "", err
-		}
-		return Helpers.IntToString(websocketConnectionGroupCount), nil
 	}
 	return commands
 }
