b11c83a6042102ad2da33726e4a3d995f51c7da2
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-01 22:04:17 +0200

    wip

--- SystemgeConnection/messageHandler.go
+++ SystemgeConnection/messageHandler.go
@@ -2,35 +2,7 @@ package SystemgeConnection
 
 import (
 	"github.com/neutralusername/Systemge/Message"
-	"github.com/neutralusername/Systemge/Metrics"
 )
-
-type MessageHandler interface {
-	HandleAsyncMessage(connection SystemgeConnection, message *Message.Message) error
-	HandleSyncRequest(connection SystemgeConnection, message *Message.Message) (string, error)
-	AddAsyncMessageHandler(topic string, handler AsyncMessageHandler)
-	AddSyncMessageHandler(topic string, handler SyncMessageHandler)
-	RemoveAsyncMessageHandler(topic string)
-	RemoveSyncMessageHandler(topic string)
-	SetUnknownAsyncHandler(handler AsyncMessageHandler)
-	SetUnknownSyncHandler(handler SyncMessageHandler)
-	GetAsyncMessageHandler(topic string) AsyncMessageHandler
-	GetSyncMessageHandler(topic string) SyncMessageHandler
-	GetAsyncTopics() []string
-	GetSyncTopics() []string
-
-	CheckMetrics() Metrics.MetricsTypes
-	GetMetrics() Metrics.MetricsTypes
-
-	CheckAsyncMessagesHandled() uint64
-	GetAsyncMessagesHandled() uint64
-
-	CheckSyncRequestsHandled() uint64
-	GetSyncRequestsHandled() uint64
-
-	CheckUnknownTopicsReceived() uint64
-	GetUnknownTopicsReceived() uint64
-}
 
 type AsyncMessageHandler func(SystemgeConnection, *Message.Message)
 type AsyncMessageHandlers map[string]AsyncMessageHandler
