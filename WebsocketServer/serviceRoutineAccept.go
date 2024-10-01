7a01d70923ccc2bb9ac2ace537adb5b242089df7
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-01 14:14:23 +0200

    wip

--- WebsocketServer/serviceRoutineAccept.go
+++ WebsocketServer/serviceRoutineAccept.go
@@ -81,48 +81,15 @@ func (server *WebsocketServer) handleNewSession() error {
 }
 
 func (server *WebsocketServer) websocketConnectionDisconnect(session *SessionManager.Session, websocketConnection *WebsocketConnection) {
-	defer server.waitGroup.Done()
-
 	select {
 	case <-websocketConnection.stopChannel:
+	case <-session.GetTimeout().GetTriggeredChannel():
 	case <-server.stopChannel:
-		websocketConnection.Close()
 	}
+
+	websocketConnection.Close()
+	session.GetTimeout().Trigger()
 
 	websocketConnection.waitGroup.Wait()
-
-	server.onEvent(Event.NewInfoNoOption(
-		Event.HandlingDisconnection,
-		"disconnecting websocketConnection",
-		Event.Context{
-			Event.Circumstance: Event.HandleDisconnection,
-			Event.IdentityType: Event.WebsocketConnection,
-			Event.Identity:     session.GetIdentity(),
-			Event.Address:      websocketConnection.GetAddress(),
-		},
-	))
-	server.removeWebsocketConnection(websocketConnection)
-
-	server.onEvent(Event.NewInfoNoOption(
-		Event.HandledDisconnection,
-		"websocketConnection disconnected",
-		Event.Context{
-			Event.Circumstance: Event.HandleDisconnection,
-			Event.IdentityType: Event.WebsocketConnection,
-			Event.ClientId:     websocketConnection.GetId(),
-			Event.Address:      websocketConnection.GetAddress(),
-		},
-	))
-}
-func (server *WebsocketServer) removeWebsocketConnection(websocketConnection *WebsocketConnection) {
-	server.websocketConnectionMutex.Lock()
-	defer server.websocketConnectionMutex.Unlock()
-	delete(server.websocketConnections, websocketConnection.GetId())
-	for groupId := range server.websocketConnectionGroups[websocketConnection.GetId()] {
-		delete(server.websocketConnectionGroups[websocketConnection.GetId()], groupId)
-		delete(server.groupsWebsocketConnections[groupId], websocketConnection.GetId())
-		if len(server.groupsWebsocketConnections[groupId]) == 0 {
-			delete(server.groupsWebsocketConnections, groupId)
-		}
-	}
+	server.waitGroup.Done()
 }
