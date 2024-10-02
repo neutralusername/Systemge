345dea0ec52e88688d0af698a39711d585a1a2f5
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-02 10:36:06 +0200

    wip

--- WebsocketServer/serviceRoutineMessagesReception.go
+++ WebsocketServer/serviceRoutineMessagesReception.go
@@ -73,27 +73,30 @@ func (server *WebsocketServer) messageReceptionRoutine(websocketConnection *Webs
 }
 
 func (server *WebsocketServer) handleMessageReception(websocketConnection *WebsocketConnection, messageBytes []byte, behaviour string) {
-	result, err := websocketConnection.receptionManager.HandleReception(messageBytes, websocketConnection)
-	if err != nil {
-		if server.eventHandler != nil {
-			server.onEvent(Event.NewWarningNoOption(
-				Event.PipelineFailed,
-				err.Error(),
-				Event.Context{
-					Event.Circumstance: Event.HandleReception,
-					Event.Behaviour:    behaviour,
-					Event.SessionId:    websocketConnection.GetId(),
-					Event.Address:      websocketConnection.GetAddress(),
-					Event.Bytes:        string(messageBytes),
-				},
-			))
+	if websocketConnection.byteRateLimiter != nil {
+		if !websocketConnection.byteRateLimiter.Consume(uint64(len(messageBytes))) {
+			return errors.New("rate limit exceeded")
 		}
-		if server.config.PropagateMessageHandlerErrors {
-			server.write(websocketConnection, Message.NewAsync("error", err.Error()).Serialize(), Event.MessageReceptionRoutine)
-		}
-		return err
 	}
-	message := result.(*Message.Message)
+	if websocketConnection.messageRateLimiter != nil {
+		if !websocketConnection.messageRateLimiter.Consume(1) {
+			return errors.New("rate limit exceeded")
+		}
+	}
+	message, err := Message.Deserialize(messageBytes, websocketConnection.GetId())
+
+	if len(message.GetSyncToken()) != 0 {
+		return errors.New("message contains sync token")
+	}
+	if len(message.GetTopic()) == 0 {
+		return errors.New("message missing topic")
+	}
+	if maxTopicSize := server.config.MaxTopicSize; maxTopicSize > 0 && len(message.GetTopic()) > maxTopicSize {
+		return errors.New("message topic exceeds maximum size")
+	}
+	if maxPayloadSize := server.config.MaxPayloadSize; maxPayloadSize > 0 && len(message.GetPayload()) > maxPayloadSize {
+		return errors.New("message payload exceeds maximum size")
+	}
 
 	_, err = server.topicManager.HandleTopic(message.GetTopic(), websocketConnection, message)
 	if err != nil {
@@ -102,7 +105,6 @@ func (server *WebsocketServer) handleMessageReception(websocketConnection *Webso
 		}
 	}
 
-	return err
 }
 
 func (server *WebsocketServer) toTopicHandler(handler WebsocketMessageHandler) TopicManager.TopicHandler {
@@ -159,38 +161,4 @@ func (server *WebsocketServer) toTopicHandler(handler WebsocketMessageHandler) T
 
 		return nil, err
 	}
-}
-
-func (server *WebsocketServer) preValidator(bytes []byte, args ...any) error {
-	websocketConnection := args[0].(*WebsocketConnection)
-	if websocketConnection.byteRateLimiter != nil {
-		if !websocketConnection.byteRateLimiter.Consume(uint64(len(bytes))) {
-			return errors.New("rate limit exceeded")
-		}
-	}
-	if websocketConnection.messageRateLimiter != nil {
-		if !websocketConnection.messageRateLimiter.Consume(1) {
-			return errors.New("rate limit exceeded")
-		}
-	}
-	return nil
-}
-func (server *WebsocketServer) deserializer(bytes []byte, args ...any) (any, error) {
-	return Message.Deserialize(bytes, args[0].(*WebsocketConnection).GetId())
-}
-func (server *WebsocketServer) postValidator(data any, args ...any) error {
-	message := data.(*Message.Message)
-	if len(message.GetSyncToken()) != 0 {
-		return errors.New("message contains sync token")
-	}
-	if len(message.GetTopic()) == 0 {
-		return errors.New("message missing topic")
-	}
-	if maxTopicSize := server.config.MaxTopicSize; maxTopicSize > 0 && len(message.GetTopic()) > maxTopicSize {
-		return errors.New("message topic exceeds maximum size")
-	}
-	if maxPayloadSize := server.config.MaxPayloadSize; maxPayloadSize > 0 && len(message.GetPayload()) > maxPayloadSize {
-		return errors.New("message payload exceeds maximum size")
-	}
-	return nil
 }
