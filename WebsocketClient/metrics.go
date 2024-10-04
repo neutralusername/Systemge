bdae1fd6c21c45cd27f9a390ef0c9e470eec03c6
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-04 18:41:31 +0200

    wip

---
+++ WebsocketClient/metrics.go
@@ -0,0 +1,1 @@
+package WebsocketClient
--- WebsocketServer/metrics.go
+++ WebsocketServer/metrics.go
@@ -14,30 +14,11 @@ func (server *WebsocketServer) CheckMetrics() Metrics.MetricsTypes {
 	)
 	metricsTypes.AddMetrics("websocketServer_messageTransmissions", Metrics.New(
 		map[string]uint64{
-			"incoming_messages": server.CheckWebsocketConnectionMessagesReceived(),
-			"outgoing_messages": server.CheckWebsocketConnectionMessagesSent(),
-			"failed_messages":   server.CheckWebsocketConnectionMessagesFailed(),
+			"messages_received": server.CheckWebsocketConnectionMessagesReceived(),
+			"messages_sent":     server.CheckWebsocketConnectionMessagesSent(),
 		}),
 	)
-
-	metricsTypes.AddMetrics("websocketServer_connections", Metrics.New(
-		map[string]uint64{
-			"connections_accepted": server.CheckWebsocketConnectionsAccepted(),
-			"connections_failed":   server.CheckWebsocketConnectionsFailed(),
-			"connections_rejected": server.CheckWebsocketConnectionsRejected(),
-		}),
-	)
-	metricsTypes.AddMetrics("websocketServer_clientSessionManager", Metrics.New(
-		map[string]uint64{
-			// todo
-		}),
-	)
-	metricsTypes.AddMetrics("websocketServer_groupSessionManager", Metrics.New(
-		map[string]uint64{
-			// todo
-		}),
-	)
-	metricsTypes.Merge(server.httpServer.CheckMetrics())
+	metricsTypes.Merge(server.websocketListener.CheckMetrics())
 	return metricsTypes
 }
 
@@ -51,84 +32,124 @@ func (server *WebsocketServer) GetMetrics() Metrics.MetricsTypes {
 	)
 	metricsTypes.AddMetrics("websocketServer_messageTransmissions", Metrics.New(
 		map[string]uint64{
-			"incoming_messages": uint64(server.GetWebsocketConnectionMessagesReceived()),
-			"outgoing_messages": uint64(server.GetWebsocketConnectionMessagesSent()),
-			"failed_messages":   uint64(server.GetWebsocketConnectionMessagesFailed()),
+			"messages_received": uint64(server.GetWebsocketConnectionMessagesReceived()),
+			"messages_sent":     uint64(server.GetWebsocketConnectionMessagesSent()),
 		}),
 	)
-	metricsTypes.AddMetrics("websocketServer_connections", Metrics.New(
-		map[string]uint64{
-			"connections_accepted": server.GetWebsocketConnectionsAccepted(),
-			"connections_failed":   server.GetWebsocketConnectionsFailed(),
-			"connections_rejected": server.GetWebsocketConnectionsRejected(),
-		}),
-	)
-	metricsTypes.AddMetrics("websocketServer_clientSessionManager", Metrics.New(
-		map[string]uint64{
-			// todo
-		}),
-	)
-	metricsTypes.AddMetrics("websocketServer_groupSessionManager", Metrics.New(
-		map[string]uint64{
-			// todo
-		}),
-	)
-	metricsTypes.Merge(server.httpServer.GetMetrics())
+	metricsTypes.Merge(server.websocketListener.GetMetrics())
 	return metricsTypes
 }
 
-func (server *WebsocketServer) GetWebsocketConnectionsAccepted() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Swap(0)
-}
-func (server *WebsocketServer) CheckWebsocketConnectionsAccepted() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Load()
-}
+/*
 
-func (server *WebsocketServer) GetWebsocketConnectionsFailed() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Swap(0)
-}
-func (server *WebsocketServer) CheckWebsocketConnectionsFailed() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Load()
-}
+	bytesSent     atomic.Uint64
+	bytesReceived atomic.Uint64
 
-func (server *WebsocketServer) GetWebsocketConnectionsRejected() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Swap(0)
-}
-func (server *WebsocketServer) CheckWebsocketConnectionsRejected() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Load()
-}
+	messagesSent     atomic.Uint64
+	messagesReceived atomic.Uint64
 
-func (server *WebsocketServer) GetWebsocketConnectionMessagesReceived() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Swap(0)
-}
-func (server *WebsocketServer) CheckWebsocketConnectionMessagesReceived() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Load()
-}
+	invalidMessagesReceived  atomic.Uint64
+	rejectedMessagesReceived atomic.Uint64
+*/
 
-func (server *WebsocketServer) GetWebsocketConnectionMessagesSent() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Swap(0)
+func (server *WebsocketServer) GetWebsocketConnectionMessagesBytesSent() uint64 {
+	websocketClients := server.GetWebsocketClients()
+	var bytesSent uint64
+	for _, websocketClient := range websocketClients {
+		bytesSent += websocketClient.GetBytesSent()
+	}
+	return bytesSent
 }
-func (server *WebsocketServer) CheckWebsocketConnectionMessagesSent() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Load()
-}
-
-func (server *WebsocketServer) GetWebsocketConnectionMessagesFailed() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Swap(0)
-}
-func (server *WebsocketServer) CheckWebsocketConnectionMessagesFailed() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Load()
+func (server *WebsocketServer) CheckWebsocketConnectionMessagesBytesSent() uint64 {
+	websocketClients := server.GetWebsocketClients()
+	var bytesSent uint64
+	for _, websocketClient := range websocketClients {
+		bytesSent += websocketClient.CheckBytesSent()
+	}
+	return bytesSent
 }
 
 func (server *WebsocketServer) GetWebsocketConnectionMessagesBytesReceived() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Swap(0)
+	websocketClients := server.GetWebsocketClients()
+	var bytesReceived uint64
+	for _, websocketClient := range websocketClients {
+		bytesReceived += websocketClient.GetBytesReceived()
+	}
+	return bytesReceived
 }
 func (server *WebsocketServer) CheckWebsocketConnectionMessagesBytesReceived() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Load()
+	websocketClients := server.GetWebsocketClients()
+	var bytesReceived uint64
+	for _, websocketClient := range websocketClients {
+		bytesReceived += websocketClient.CheckBytesReceived()
+	}
+	return bytesReceived
 }
 
-func (server *WebsocketServer) GetWebsocketConnectionMessagesBytesSent() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Swap(0)
+func (server *WebsocketServer) GetWebsocketConnectionMessagesSent() uint64 {
+	websocketClients := server.GetWebsocketClients()
+	var messagesSent uint64
+	for _, websocketClient := range websocketClients {
+		messagesSent += websocketClient.GetMessagesSent()
+	}
+	return messagesSent
 }
-func (server *WebsocketServer) CheckWebsocketConnectionMessagesBytesSent() uint64 {
-	return server.websocketConnectionMessagesBytesSent.Load()
+func (server *WebsocketServer) CheckWebsocketConnectionMessagesSent() uint64 {
+	websocketClients := server.GetWebsocketClients()
+	var messagesSent uint64
+	for _, websocketClient := range websocketClients {
+		messagesSent += websocketClient.CheckMessagesSent()
+	}
+	return messagesSent
+}
+
+func (server *WebsocketServer) GetWebsocketConnectionMessagesReceived() uint64 {
+	websocketClients := server.GetWebsocketClients()
+	var messagesReceived uint64
+	for _, websocketClient := range websocketClients {
+		messagesReceived += websocketClient.GetMessagesReceived()
+	}
+	return messagesReceived
+}
+func (server *WebsocketServer) CheckWebsocketConnectionMessagesReceived() uint64 {
+	websocketClients := server.GetWebsocketClients()
+	var messagesReceived uint64
+	for _, websocketClient := range websocketClients {
+		messagesReceived += websocketClient.CheckMessagesReceived()
+	}
+	return messagesReceived
+}
+
+func (server *WebsocketServer) GetWebsocketConnectionInvalidMessagesReceived() uint64 {
+	websocketClients := server.GetWebsocketClients()
+	var invalidMessagesReceived uint64
+	for _, websocketClient := range websocketClients {
+		invalidMessagesReceived += websocketClient.GetInvalidMessagesReceived()
+	}
+	return invalidMessagesReceived
+}
+func (server *WebsocketServer) CheckWebsocketConnectionInvalidMessagesReceived() uint64 {
+	websocketClients := server.GetWebsocketClients()
+	var invalidMessagesReceived uint64
+	for _, websocketClient := range websocketClients {
+		invalidMessagesReceived += websocketClient.CheckInvalidMessagesReceived()
+	}
+	return invalidMessagesReceived
+}
+
+func (server *WebsocketServer) GetWebsocketConnectionRejectedMessagesReceived() uint64 {
+	websocketClients := server.GetWebsocketClients()
+	var rejectedMessagesReceived uint64
+	for _, websocketClient := range websocketClients {
+		rejectedMessagesReceived += websocketClient.GetRejectedMessagesReceived()
+	}
+	return rejectedMessagesReceived
+}
+func (server *WebsocketServer) CheckWebsocketConnectionRejectedMessagesReceived() uint64 {
+	websocketClients := server.GetWebsocketClients()
+	var rejectedMessagesReceived uint64
+	for _, websocketClient := range websocketClients {
+		rejectedMessagesReceived += websocketClient.CheckRejectedMessagesReceived()
+	}
+	return rejectedMessagesReceived
 }
