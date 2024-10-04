2b374096d5982b451c81020b26373020f7b09d59
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-04 18:03:03 +0200

    wip

--- WebsocketServer/clients.go
+++ WebsocketServer/clients.go
@@ -17,3 +17,16 @@ func (server *WebsocketServer) GetWebsocketClient(sessionId string) (*WebsocketC
 	}
 	return websocketClient.(*WebsocketClient.WebsocketClient), nil
 }
+
+func (server *WebsocketServer) GetWebsocketClients() []*WebsocketClient.WebsocketClient {
+	clients := []*WebsocketClient.WebsocketClient{}
+	sessions := server.sessionManager.GetSessions("")
+	for _, session := range sessions {
+		websocketClient, ok := session.Get("websocketClient")
+		if !ok {
+			continue
+		}
+		clients = append(clients, websocketClient.(*WebsocketClient.WebsocketClient))
+	}
+	return clients
+}
