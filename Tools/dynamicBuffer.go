d79409df08f613e8abef552e28039c39e1bb252f
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-05 15:23:13 +0200

    wip

--- Tools/dynamicBuffer.go
+++ Tools/dynamicBuffer.go
@@ -1,5 +1,7 @@
 package Tools
 
+import "errors"
+
 // dynamic buffer:
 // items have an optional deadline, after which they are removed from the queue
 
@@ -62,6 +64,45 @@ func NewDynamicBuffer(capacity uint32) *DynamicBuffer {
 		}
 	}()
 	return buffer
+}
+
+func (buffer *DynamicBuffer) GetNextItem() (any, error) {
+	if buffer.head == nil {
+		return nil, errors.New("buffer is empty")
+	}
+	item := buffer.head
+
+	buffer.head = buffer.head.prev
+	if buffer.head == nil {
+		buffer.tail = nil
+		return item.item, nil
+	}
+	if item.prev != nil {
+		item.prev.next = nil
+	}
+	delete(buffer.items, item.token)
+	return item.item, nil
+}
+
+func (buffer *DynamicBuffer) GetItemByToken(token string) (any, error) {
+	item := buffer.items[token]
+	if item == nil {
+		return nil, errors.New("item not found")
+	}
+	if item.prev != nil {
+		item.prev.next = item.next
+	}
+	if item.next != nil {
+		item.next.prev = item.prev
+	}
+	if buffer.head == item {
+		buffer.head = item.prev
+	}
+	if buffer.tail == item {
+		buffer.tail = item.next
+	}
+	delete(buffer.items, item.token)
+	return item.item, nil
 }
 
 func (buffer *DynamicBuffer) AddItem(token string, item any, priority uint32, deadlineMs uint64) error {
