c12258791bfdd6c944ccb2c7a8086463452bc4d1
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-05 09:26:31 +0200

    wip

--- Tools/receptionManager.go
+++ Tools/receptionManager.go
@@ -1,6 +1,6 @@
 package Tools
 
 // add message-bytes to the manager. manager checks rate limits, deserializes and validates the messages, queues it to the channel.
-// unresolved: include access control based on remaining space in the buffered channel / reconcile this with the life-cycle of the connection(tcp or ws)
-// unresolved: also include a generalized mechanism to automatically handle the messages (topic-handler, probably through a provided any-func that bridges both tools)
+// unresolved: access control based on remaining space in the buffered channel / reconcile this with the life-cycle of the connection(tcp or ws)
+// unresolved: generalized mechanism to automatically handle the messages (topic-handler, probably through a provided any-func that bridges both tools)
 // unresolved: automated sync response handling (even without automated handling of the messages ?)
