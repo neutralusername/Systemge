cad85ff54e1df1834723f0d299db52a9df5e0c85
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-05 12:43:04 +0200

    wip

--- Tools/receptionManager.go
+++ Tools/receptionManager.go
@@ -1,9 +1,26 @@
 package Tools
 
-// add message-bytes to the manager. manager checks rate limits, deserializes and validates the messages, queues it to the channel.
-// unresolved: access control based on remaining space in the buffered channel / reconcile this with the life-cycle of the connection(tcp or ws)
-// unresolved: generalized mechanism to automatically handle the messages (topic-handler, probably through a provided any-func that bridges both tools)
-// unresolved: automated sync response handling (even without automated handling of the messages ?)
-// unresolved: (maybe) implement it in a way in which it does not explicitly use messages, but rather any
+// queuemanger:
+// items have an optional deadline, after which they are removed from the queue
 
-// maybe make two separate tools - queue and automated handling with this queue
+// Add (any, priority, deadlineMs) (token, error)
+
+// GetItem (order/priority)
+// GetItemFIFO
+// GetItemLIFO
+// GetItemByToken
+//
+
+type QueueManager struct {
+	channel chan any
+}
+
+func NewReceptionManager(capacity uint32) *QueueManager {
+	return &QueueManager{
+		channel: make(chan any, capacity),
+	}
+}
+
+func (manager *QueueManager) Add(item any) (string, error) {
+
+}
