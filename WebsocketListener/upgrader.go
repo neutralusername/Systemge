172b82fcc998d1de99d33cc1e833838be139446e
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-06 22:56:06 +0200

    wip

--- WebsocketListener/upgrader.go
+++ WebsocketListener/upgrader.go
@@ -5,37 +5,37 @@ import (
 	"time"
 )
 
-func (server *WebsocketListener) getHTTPWebsocketUpgradeHandler() http.HandlerFunc {
+func (listener *WebsocketListener) getHTTPWebsocketUpgradeHandler() http.HandlerFunc {
 	return func(responseWriter http.ResponseWriter, httpRequest *http.Request) {
 		var deadline <-chan time.Time
-		if server.config.WebsocketRequestTimeoutMs > 0 {
-			deadline = time.After(time.Duration(server.config.WebsocketRequestTimeoutMs) * time.Millisecond)
+		if listener.config.WebsocketRequestTimeoutMs > 0 {
+			deadline = time.After(time.Duration(listener.config.WebsocketRequestTimeoutMs) * time.Millisecond)
 		}
 		select {
 		case <-deadline:
 			http.Error(responseWriter, "Request timeout", http.StatusRequestTimeout)
-			server.ClientsRejected.Add(1)
+			listener.ClientsRejected.Add(1)
 			return
 
-		case <-server.stopChannel:
+		case <-listener.stopChannel:
 			http.Error(responseWriter, "Internal server error", http.StatusInternalServerError)
-			server.ClientsRejected.Add(1)
+			listener.ClientsRejected.Add(1)
 			return
 
-		case acceptRequest := <-server.pool.AcquireItemChannel():
-			websocketConn, err := server.config.Upgrader.Upgrade(responseWriter, httpRequest, nil)
+		case acceptRequest := <-listener.pool.AcquireItemChannel():
+			websocketConn, err := listener.config.Upgrader.Upgrade(responseWriter, httpRequest, nil)
 			acceptRequest.upgraderResponseChannel <- &upgraderResponse{
 				err:           err,
 				websocketConn: websocketConn,
 			}
 			if err != nil {
-				server.ClientsFailed.Add(1)
+				listener.ClientsFailed.Add(1)
 			} else {
 				acceptRequest.triggered.Wait()
 				select {
 				case <-acceptRequest.upgraderResponseChannel:
 					websocketConn.Close()
-					server.ClientsFailed.Add(1)
+					listener.ClientsFailed.Add(1)
 				default:
 				}
 			}
