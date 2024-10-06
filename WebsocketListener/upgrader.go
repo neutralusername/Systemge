8f601ac89cfb225493ef8599a96f7b633f58858e
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-06 17:41:45 +0200

    wip

--- WebsocketListener/upgrader.go
+++ WebsocketListener/upgrader.go
@@ -12,24 +12,31 @@ func (server *WebsocketListener) getHTTPWebsocketUpgradeHandler() http.HandlerFu
 				http.Error(responseWriter, "Internal server error", http.StatusInternalServerError)
 				server.ClientsRejected.Add(1)
 				return
-			case acceptRequest := <-server.acceptChannel:
-				if acceptRequest.timedOut {
-					continue
-				}
-
-				websocketConn, err := server.config.Upgrader.Upgrade(responseWriter, httpRequest, nil)
-
-				acceptRequest.upgraderResponseChannel <- &upgraderResponse{
-					err:           err,
-					websocketConn: websocketConn,
-				}
-				if err != nil {
-					server.ClientsFailed.Add(1)
-				} else {
-					acceptRequest.triggered.Wait()
+			default:
+				select {
+				case <-server.stopChannel:
+					http.Error(responseWriter, "Internal server error", http.StatusInternalServerError)
+					server.ClientsRejected.Add(1)
+					return
+				case acceptRequest := <-server.acceptChannel:
 					if acceptRequest.timedOut {
-						websocketConn.Close()
+						continue
+					}
+
+					websocketConn, err := server.config.Upgrader.Upgrade(responseWriter, httpRequest, nil)
+
+					acceptRequest.upgraderResponseChannel <- &upgraderResponse{
+						err:           err,
+						websocketConn: websocketConn,
+					}
+					if err != nil {
 						server.ClientsFailed.Add(1)
+					} else {
+						acceptRequest.triggered.Wait()
+						if acceptRequest.timedOut {
+							websocketConn.Close()
+							server.ClientsFailed.Add(1)
+						}
 					}
 				}
 			}
