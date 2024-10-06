d73726bc27d2e9e85cf009dac918d1c1a3d2dec6
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-06 15:25:50 +0200

    wip

--- WebsocketListener/accept.go
+++ WebsocketListener/accept.go
@@ -1,19 +1,20 @@
 package WebsocketListener
 
 import (
-	"errors"
-
 	"github.com/neutralusername/Systemge/Config"
 	"github.com/neutralusername/Systemge/WebsocketClient"
 )
 
 func (listener *WebsocketListener) Accept(config *Config.WebsocketClient) (*WebsocketClient.WebsocketClient, error) {
-	websocketConn := <-listener.connectionChannel
-	if websocketConn == nil {
-		return nil, errors.New("received nil value from connection channel")
+	upgraderResponseChannel := make(chan *upgraderResponse)
+	listener.acceptChannel <- upgraderResponseChannel
+	upgraderResponse := <-upgraderResponseChannel
+
+	if upgraderResponse.err != nil {
+		return nil, upgraderResponse.err
 	}
 
-	websocketClient, err := WebsocketClient.New(config, websocketConn)
+	websocketClient, err := WebsocketClient.New(config, upgraderResponse.websocketConn)
 	if err != nil {
 		listener.ClientsFailed.Add(1)
 		return nil, err
