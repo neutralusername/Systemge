eb42e6297fc69d9b0fd755abf63c5b69a90d3347
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-07 13:51:49 +0200

    wip

--- WebsocketListener/upgrader.go
+++ WebsocketListener/upgrader.go
@@ -2,27 +2,23 @@ package WebsocketListener
 
 import (
 	"net/http"
-	"time"
 )
 
 func (listener *WebsocketListener) getHTTPWebsocketUpgradeHandler() http.HandlerFunc {
 	return func(responseWriter http.ResponseWriter, httpRequest *http.Request) {
-		var deadline <-chan time.Time
-		if listener.config.WebsocketRequestTimeoutMs > 0 {
-			deadline = time.After(time.Duration(listener.config.WebsocketRequestTimeoutMs) * time.Millisecond)
-		}
 		select {
-		case <-deadline:
-			http.Error(responseWriter, "Request timeout", http.StatusRequestTimeout)
-			listener.ClientsRejected.Add(1)
-			return
-
 		case <-listener.stopChannel:
 			http.Error(responseWriter, "Internal server error", http.StatusInternalServerError)
 			listener.ClientsRejected.Add(1)
 			return
 
-		case acceptRequest := <-listener.pool.AcquireItemChannel():
+		case acceptRequest, ok := <-listener.pool.AcquireItemChannel(listener.config.WebsocketRequestTimeoutMs):
+			if !ok {
+				http.Error(responseWriter, "Request timeout", http.StatusRequestTimeout)
+				listener.ClientsRejected.Add(1)
+				return
+			}
+
 			websocketConn, err := listener.config.Upgrader.Upgrade(responseWriter, httpRequest, nil)
 			acceptRequest.upgraderResponseChannel <- &upgraderResponse{
 				err:           err,
