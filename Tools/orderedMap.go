53922bc3170f0b133fcd5c199d4591ed32e04f1e
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-07 11:00:12 +0200

    wip

--- Tools/orderedMap.go
+++ Tools/orderedMap.go
@@ -5,23 +5,23 @@ import (
 	"sync"
 )
 
-type OrderedMap[K comparable, V any] struct {
-	head   *orderedMapNode[K, V]
-	tail   *orderedMapNode[K, V]
-	values map[K]*orderedMapNode[K, V]
+type KeyDequeue[K comparable, V any] struct {
+	head   *keyDequeueNode[K, V]
+	tail   *keyDequeueNode[K, V]
+	values map[K]*keyDequeueNode[K, V]
 	mutex  sync.RWMutex
 }
 
-type orderedMapNode[K comparable, V any] struct {
+type keyDequeueNode[K comparable, V any] struct {
 	key   K
 	value V
-	next  *orderedMapNode[K, V]
-	prev  *orderedMapNode[K, V]
+	next  *keyDequeueNode[K, V]
+	prev  *keyDequeueNode[K, V]
 }
 
-func NewOrderedMap[K comparable, V any](initialItems map[K]V) *OrderedMap[K, V] {
-	orderedMap := &OrderedMap[K, V]{
-		values: make(map[K]*orderedMapNode[K, V]),
+func NewKeyDequeue[K comparable, V any](initialItems map[K]V) *KeyDequeue[K, V] {
+	orderedMap := &KeyDequeue[K, V]{
+		values: make(map[K]*keyDequeueNode[K, V]),
 	}
 	for key, value := range initialItems {
 		orderedMap.Add(key, value)
@@ -29,103 +29,103 @@ func NewOrderedMap[K comparable, V any](initialItems map[K]V) *OrderedMap[K, V] 
 	return orderedMap
 }
 
-func newOrderedMapNode[K comparable, V any](key K, value V) *orderedMapNode[K, V] {
-	return &orderedMapNode[K, V]{
+func newKeyDequeueNode[K comparable, V any](key K, value V) *keyDequeueNode[K, V] {
+	return &keyDequeueNode[K, V]{
 		key:   key,
 		value: value,
 	}
 }
 
-func (orderedMap *OrderedMap[K, V]) Add(key K, value V) error {
-	orderedMap.mutex.Lock()
-	defer orderedMap.mutex.Unlock()
+func (keyDequeue *KeyDequeue[K, V]) Add(key K, value V) error {
+	keyDequeue.mutex.Lock()
+	defer keyDequeue.mutex.Unlock()
 
-	if _, ok := orderedMap.values[key]; ok {
+	if _, ok := keyDequeue.values[key]; ok {
 		return errors.New("key already exists")
 	}
 
-	node := newOrderedMapNode(key, value)
-	orderedMap.values[key] = node
-	if orderedMap.head == nil {
-		orderedMap.head = node
-		orderedMap.tail = node
+	node := newKeyDequeueNode(key, value)
+	keyDequeue.values[key] = node
+	if keyDequeue.head == nil {
+		keyDequeue.head = node
+		keyDequeue.tail = node
 		return nil
 	}
 
-	orderedMap.tail.next = node
-	node.prev = orderedMap.tail
-	orderedMap.tail = node
+	keyDequeue.tail.next = node
+	node.prev = keyDequeue.tail
+	keyDequeue.tail = node
 	return nil
 }
 
-func (orderedMap *OrderedMap[K, V]) RetrieveFIFO() (K, V, error) {
-	orderedMap.mutex.Lock()
-	defer orderedMap.mutex.Unlock()
+func (keyDequeue *KeyDequeue[K, V]) RetrieveFIFO() (K, V, error) {
+	keyDequeue.mutex.Lock()
+	defer keyDequeue.mutex.Unlock()
 
-	if orderedMap.head == nil {
+	if keyDequeue.head == nil {
 		var nilKey K
 		var nilValue V
 		return nilKey, nilValue, errors.New("empty map")
 	}
-	node := orderedMap.head
-	delete(orderedMap.values, node.key)
-	orderedMap.head = node.next
+	node := keyDequeue.head
+	delete(keyDequeue.values, node.key)
+	keyDequeue.head = node.next
 	if node.next != nil {
 		node.next.prev = nil
 	} else {
-		orderedMap.tail = nil
+		keyDequeue.tail = nil
 	}
 	return node.key, node.value, nil
 }
 
-func (orderedMap *OrderedMap[K, V]) RetrieveLIFO() (K, V, error) {
-	orderedMap.mutex.Lock()
-	defer orderedMap.mutex.Unlock()
+func (keyDequeue *KeyDequeue[K, V]) RetrieveLIFO() (K, V, error) {
+	keyDequeue.mutex.Lock()
+	defer keyDequeue.mutex.Unlock()
 
-	if orderedMap.tail == nil {
+	if keyDequeue.tail == nil {
 		var nilKey K
 		var nilValue V
 		return nilKey, nilValue, errors.New("empty map")
 	}
-	node := orderedMap.tail
-	delete(orderedMap.values, node.key)
-	orderedMap.tail = node.prev
+	node := keyDequeue.tail
+	delete(keyDequeue.values, node.key)
+	keyDequeue.tail = node.prev
 	if node.prev != nil {
 		node.prev.next = nil
 	} else {
-		orderedMap.head = nil
+		keyDequeue.head = nil
 	}
 	return node.key, node.value, nil
 }
 
-func (orderedMap *OrderedMap[K, V]) RetrieveKey(key K) (V, error) {
-	orderedMap.mutex.Lock()
-	defer orderedMap.mutex.Unlock()
+func (keyDequeue *KeyDequeue[K, V]) RetrieveKey(key K) (V, error) {
+	keyDequeue.mutex.Lock()
+	defer keyDequeue.mutex.Unlock()
 
-	node, ok := orderedMap.values[key]
+	node, ok := keyDequeue.values[key]
 	if !ok {
 		var nilValue V
 		return nilValue, errors.New("key not found")
 	}
-	delete(orderedMap.values, key)
+	delete(keyDequeue.values, key)
 	if node.prev != nil {
 		node.prev.next = node.next
 	} else {
-		orderedMap.head = node.next
+		keyDequeue.head = node.next
 	}
 	if node.next != nil {
 		node.next.prev = node.prev
 	} else {
-		orderedMap.tail = node.prev
+		keyDequeue.tail = node.prev
 	}
 	return node.value, nil
 }
 
-func (orderedMap *OrderedMap[K, V]) UpdateValue(key K, value V) error {
-	orderedMap.mutex.Lock()
-	defer orderedMap.mutex.Unlock()
+func (keyDequeue *KeyDequeue[K, V]) UpdateValue(key K, value V) error {
+	keyDequeue.mutex.Lock()
+	defer keyDequeue.mutex.Unlock()
 
-	node, ok := orderedMap.values[key]
+	node, ok := keyDequeue.values[key]
 	if !ok {
 		return errors.New("key not found")
 	}
@@ -135,11 +135,11 @@ func (orderedMap *OrderedMap[K, V]) UpdateValue(key K, value V) error {
 
 // returns the value associated with the provided key.
 // returns an error if the key is not found.
-func (orderedMap *OrderedMap[K, V]) GetValue(key K) (V, error) {
-	orderedMap.mutex.RLock()
-	defer orderedMap.mutex.RUnlock()
+func (keyDequeue *KeyDequeue[K, V]) GetValue(key K) (V, error) {
+	keyDequeue.mutex.RLock()
+	defer keyDequeue.mutex.RUnlock()
 
-	if node, ok := orderedMap.values[key]; ok {
+	if node, ok := keyDequeue.values[key]; ok {
 		return node.value, nil
 	}
 	var nilValue V
@@ -147,12 +147,12 @@ func (orderedMap *OrderedMap[K, V]) GetValue(key K) (V, error) {
 }
 
 // returns the keys in the order they were pushed
-func (orderedMap *OrderedMap[K, V]) GetKeys() []K {
-	orderedMap.mutex.RLock()
-	defer orderedMap.mutex.RUnlock()
+func (keyDequeue *KeyDequeue[K, V]) GetKeys() []K {
+	keyDequeue.mutex.RLock()
+	defer keyDequeue.mutex.RUnlock()
 
-	keys := make([]K, 0, len(orderedMap.values))
-	currentNode := orderedMap.head
+	keys := make([]K, 0, len(keyDequeue.values))
+	currentNode := keyDequeue.head
 	for currentNode != nil {
 		keys = append(keys, currentNode.key)
 		currentNode = currentNode.next
@@ -161,12 +161,12 @@ func (orderedMap *OrderedMap[K, V]) GetKeys() []K {
 }
 
 // returns the values in the order they were pushed
-func (orderedMap *OrderedMap[K, V]) GetValues() []V {
-	orderedMap.mutex.RLock()
-	defer orderedMap.mutex.RUnlock()
+func (keyDequeue *KeyDequeue[K, V]) GetValues() []V {
+	keyDequeue.mutex.RLock()
+	defer keyDequeue.mutex.RUnlock()
 
-	values := make([]V, 0, len(orderedMap.values))
-	currentNode := orderedMap.head
+	values := make([]V, 0, len(keyDequeue.values))
+	currentNode := keyDequeue.head
 	for currentNode != nil {
 		values = append(values, currentNode.value)
 		currentNode = currentNode.next
