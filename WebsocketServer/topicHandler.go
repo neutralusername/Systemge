9072330a10c0294e13a3e04a1b0380823a5b61a2
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-09 12:10:58 +0200

    wip

---
+++ WebsocketServer/topicHandler.go
@@ -0,0 +1,15 @@
+package WebsocketServer
+
+import (
+	"github.com/neutralusername/Systemge/Config"
+	"github.com/neutralusername/Systemge/Message"
+	"github.com/neutralusername/Systemge/Tools"
+	"github.com/neutralusername/Systemge/WebsocketClient"
+)
+
+type TopicHandler func(message *Message.Message, websocketServer *WebsocketServer, websocketClient *WebsocketClient.WebsocketClient, identity, sessionId string) error
+
+func NewWebsocketTopicManager(config *Config.TopicManager, topicHandlers map[string]TopicHandler, unknownTopicHandler TopicHandler) *Tools.TopicManager {
+
+	return Tools.NewTopicManager(config, topicHandlers, unknownTopicHandler)
+}
