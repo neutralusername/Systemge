c55c8ddee4868d03d1902858b792b8280ef4a028
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-09 12:15:31 +0200

    wip

--- WebsocketServer/topicHandler.go
+++ WebsocketServer/topicHandler.go
@@ -9,7 +9,25 @@ import (
 
 type WebsocketTopicHandler func(message *Message.Message, websocketServer *WebsocketServer, websocketClient *WebsocketClient.WebsocketClient, identity, sessionId string) error
 
-func NewWebsocketTopicManager(config *Config.TopicManager, topicHandlers map[string]WebsocketTopicHandler, unknownTopicHandler WebsocketTopicHandler) *Tools.TopicManager {
-
-	return Tools.NewTopicManager(config, convertedTopicHandlers, convertedUnknownTopicHandler)
+func NewWebsocketTopicManager(config *Config.TopicManager, websocketTopicHandlers map[string]WebsocketTopicHandler, unknownWebsocketTopicHandler WebsocketTopicHandler) *Tools.TopicManager {
+	topicHandlers := make(Tools.TopicHandlers)
+	for topic, handler := range websocketTopicHandlers {
+		topicHandlers[topic] = func(args ...any) (any, error) {
+			message := args[0].(*Message.Message)
+			websocketServer := args[1].(*WebsocketServer)
+			websocketClient := args[2].(*WebsocketClient.WebsocketClient)
+			identity := args[3].(string)
+			sessionId := args[4].(string)
+			return nil, handler(message, websocketServer, websocketClient, identity, sessionId)
+		}
+	}
+	unknownTopicHandler := func(args ...any) (any, error) {
+		message := args[0].(*Message.Message)
+		websocketServer := args[1].(*WebsocketServer)
+		websocketClient := args[2].(*WebsocketClient.WebsocketClient)
+		identity := args[3].(string)
+		sessionId := args[4].(string)
+		return nil, unknownWebsocketTopicHandler(message, websocketServer, websocketClient, identity, sessionId)
+	}
+	return Tools.NewTopicManager(config, topicHandlers, unknownTopicHandler)
 }
