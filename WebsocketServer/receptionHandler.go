f789582ece2f335e9bf058022dbe2ea1a0ea0a9b
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-11 16:23:44 +0200

    wip

--- WebsocketServer/receptionHandler.go
+++ WebsocketServer/receptionHandler.go
@@ -2,7 +2,6 @@ package WebsocketServer
 
 import (
 	"errors"
-	"sync"
 
 	"github.com/neutralusername/Systemge/Config"
 	"github.com/neutralusername/Systemge/Message"
@@ -96,68 +95,6 @@ func NewValidationMessageReceptionHandlerFactory(byteRateLimiterConfig *Config.T
 			return nil
 		}
 		return messageHandler(message, websocketServer, websocketClient, identity, sessionId)
-	}
-
-	if topicManager != nil {
-		handleTopic = func(message *Message.Message, websocketServer *WebsocketServer[*Message.Message], websocketClient *WebsocketClient.WebsocketClient, identity, sessionId string) error {
-			// event
-
-			response, err := topicManager.Handle(message.GetTopic(), message, websocketServer, websocketClient, identity, sessionId)
-			if err != nil {
-				// event
-				return err
-			}
-
-			if response != nil {
-				message, ok := response.(*Message.Message)
-				if !ok {
-					// event
-					return errors.New("invalid response type")
-				}
-				if err := websocketClient.Write(message.Serialize(), websocketServer.config.WriteTimeoutMs); err != nil {
-					// event
-				}
-			}
-			// event
-			return nil
-		}
-	}
-	if priorityQueue != nil {
-		if topicManager != nil {
-			websocketReceptionHandlerInitFunc = func(websocketServer *WebsocketServer[*Message.Message], websocketClient *WebsocketClient.WebsocketClient, identity, sessionId string) {
-				go func() {
-					for {
-						select {
-						case message := <-priorityQueue.PopChannel():
-							handleTopic(message, websocketServer, websocketClient, identity, sessionId)
-						case <-websocketClient.GetCloseChannel():
-							if priorityQueue.Len() == 0 {
-								return
-							}
-						}
-					}
-				}()
-			}
-		}
-		mutex := &sync.Mutex{}
-		messageHandler = func(message *Message.Message, websocketServer *WebsocketServer[*Message.Message], websocketClient *WebsocketClient.WebsocketClient, identity, sessionId string) error {
-			mutex.Lock()
-			priority := topicPriorities[message.GetTopic()]
-			timeoutMs := topicTimeoutMs[message.GetTopic()]
-			mutex.Unlock()
-			// event
-			return priorityQueue.Push("", message, priority, timeoutMs)
-		}
-	} else {
-		if topicManager != nil {
-			messageHandler = func(message *Message.Message, websocketServer *WebsocketServer[*Message.Message], websocketClient *WebsocketClient.WebsocketClient, identity, sessionId string) error {
-				return handleTopic(message, websocketServer, websocketClient, identity, sessionId)
-			}
-		} else {
-			messageHandler = func(message *Message.Message, websocketServer *WebsocketServer[*Message.Message], websocketClient *WebsocketClient.WebsocketClient, identity, sessionId string) error {
-				return nil
-			}
-		}
 	}
 
 	return NewValidationReceptionHandlerFactory(byteRateLimiterConfig, messageRateLimiterConfig, objectDeserializer, objectHandler, websocketReceptionHandlerInitFunc)
