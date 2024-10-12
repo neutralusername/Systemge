1f3903fcde1a4a6e54538facece342613ba5273b
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-12 12:58:24 +0200

    wip

--- WebsocketServer/receptionHandler.go
+++ WebsocketServer/receptionHandler.go
@@ -34,27 +34,29 @@ func NewWebsocketMessageReceptionManagerFactory(
 	}
 
 	objectHandlers := []Tools.ObjectHandler[*Message.Message, *websocketServerReceptionManagerCaller]{}
-	objectHandlers = append(objectHandlers, Tools.NewValidationObjectHandler(func(message *Message.Message, caller *websocketServerReceptionManagerCaller) error {
-		if messageValidatorConfig.MinSyncTokenSize >= 0 && len(message.GetSyncToken()) < messageValidatorConfig.MinSyncTokenSize {
-			return errors.New("message contains sync token")
-		}
-		if messageValidatorConfig.MaxSyncTokenSize >= 0 && len(message.GetSyncToken()) > messageValidatorConfig.MaxSyncTokenSize {
-			return errors.New("message contains sync token")
-		}
-		if messageValidatorConfig.MinTopicSize >= 0 && len(message.GetTopic()) < messageValidatorConfig.MinTopicSize {
-			return errors.New("message missing topic")
-		}
-		if messageValidatorConfig.MaxTopicSize >= 0 && len(message.GetTopic()) > messageValidatorConfig.MaxTopicSize {
-			return errors.New("message missing topic")
-		}
-		if messageValidatorConfig.MinPayloadSize >= 0 && len(message.GetPayload()) < messageValidatorConfig.MinPayloadSize {
-			return errors.New("message payload exceeds maximum size")
-		}
-		if messageValidatorConfig.MaxPayloadSize >= 0 && len(message.GetPayload()) > messageValidatorConfig.MaxPayloadSize {
-			return errors.New("message payload exceeds maximum size")
-		}
-		return nil
-	}))
+	if messageValidatorConfig != nil {
+		objectHandlers = append(objectHandlers, Tools.NewValidationObjectHandler(func(message *Message.Message, caller *websocketServerReceptionManagerCaller) error {
+			if messageValidatorConfig.MinSyncTokenSize >= 0 && len(message.GetSyncToken()) < messageValidatorConfig.MinSyncTokenSize {
+				return errors.New("message contains sync token")
+			}
+			if messageValidatorConfig.MaxSyncTokenSize >= 0 && len(message.GetSyncToken()) > messageValidatorConfig.MaxSyncTokenSize {
+				return errors.New("message contains sync token")
+			}
+			if messageValidatorConfig.MinTopicSize >= 0 && len(message.GetTopic()) < messageValidatorConfig.MinTopicSize {
+				return errors.New("message missing topic")
+			}
+			if messageValidatorConfig.MaxTopicSize >= 0 && len(message.GetTopic()) > messageValidatorConfig.MaxTopicSize {
+				return errors.New("message missing topic")
+			}
+			if messageValidatorConfig.MinPayloadSize >= 0 && len(message.GetPayload()) < messageValidatorConfig.MinPayloadSize {
+				return errors.New("message payload exceeds maximum size")
+			}
+			if messageValidatorConfig.MaxPayloadSize >= 0 && len(message.GetPayload()) > messageValidatorConfig.MaxPayloadSize {
+				return errors.New("message payload exceeds maximum size")
+			}
+			return nil
+		}))
+	}
 	if requestResponseManager != nil && obtainResponseToken != nil {
 		objectHandlers = append(objectHandlers, Tools.NewResponseObjectHandler(requestResponseManager, obtainResponseToken))
 	}
