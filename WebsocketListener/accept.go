f2c883794ad59b67295b1cf3f7b33fba8ed2dfa0
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-13 15:43:28 +0200

    wip

--- WebsocketListener/accept.go
+++ WebsocketListener/accept.go
@@ -54,7 +54,7 @@ func (listener *WebsocketListener) SetAcceptDeadline(timeoutMs uint32) {
 
 }
 
-func (listener *WebsocketListener) Accept(config *Config.WebsocketClient, timeoutMs uint32) (*WebsocketClient.WebsocketClient, error) {
+func (listener *WebsocketListener) Accept(config *Config.WebsocketClient) (*WebsocketClient.WebsocketClient, error) {
 	listener.mutex.RLock()
 	if listener.status != Status.Started {
 		listener.mutex.RUnlock()
@@ -64,41 +64,59 @@ func (listener *WebsocketListener) Accept(config *Config.WebsocketClient, timeou
 	defer listener.waitgroup.Done()
 	listener.mutex.RUnlock()
 
-	acceptRequest := &acceptRequest{
-		upgraderResponseChannel: make(chan *upgraderResponse),
-		triggered:               sync.WaitGroup{},
-	}
-	acceptRequest.triggered.Add(1)
-	listener.pool.AddItems(true, acceptRequest)
-
-	var deadline <-chan time.Time
-	if timeoutMs > 0 {
-		deadline = time.After(time.Duration(timeoutMs) * time.Millisecond)
-	}
-	select {
-	case <-listener.stopChannel:
-		listener.pool.RemoveItems(true, acceptRequest)
-		acceptRequest.triggered.Done()
-		return nil, errors.New("listener stopped")
-
-	case <-deadline:
-		listener.pool.RemoveItems(true, acceptRequest)
-		acceptRequest.triggered.Done()
-		return nil, errors.New("timeout")
-
-	case upgraderResponse := <-acceptRequest.upgraderResponseChannel:
-		listener.pool.RemoveItems(true, acceptRequest)
-		acceptRequest.triggered.Done()
-		if upgraderResponse.err != nil {
-			return nil, upgraderResponse.err
-		}
-		websocketClient, err := WebsocketClient.New(config, upgraderResponse.websocketConn)
-		if err != nil {
-			listener.ClientsFailed.Add(1)
-			upgraderResponse.websocketConn.Close()
-			return nil, err
-		}
-		listener.ClientsAccepted.Add(1)
-		return websocketClient, nil
-	}
+	return listener.accept(config)
 }
+
+func (listener *WebsocketListener) AcceptTimeout(config *Config.WebsocketClient, timeoutMs uint32) (*WebsocketClient.WebsocketClient, error) {
+	listener.mutex.RLock()
+	if listener.status != Status.Started {
+		listener.mutex.RUnlock()
+		return nil, errors.New("listener not started")
+	}
+	listener.waitgroup.Add(1)
+	defer listener.waitgroup.Done()
+	listener.mutex.RUnlock()
+
+	listener.SetAcceptDeadline(timeoutMs)
+	return listener.accept(config)
+}
+
+/*
+acceptRequest := &acceptRequest{
+	upgraderResponseChannel: make(chan *upgraderResponse),
+	triggered:               sync.WaitGroup{},
+}
+acceptRequest.triggered.Add(1)
+listener.pool.AddItems(true, acceptRequest)
+
+var deadline <-chan time.Time
+if timeoutMs > 0 {
+	deadline = time.After(time.Duration(timeoutMs) * time.Millisecond)
+}
+select {
+case <-listener.stopChannel:
+	listener.pool.RemoveItems(true, acceptRequest)
+	acceptRequest.triggered.Done()
+	return nil, errors.New("listener stopped")
+
+case <-deadline:
+	listener.pool.RemoveItems(true, acceptRequest)
+	acceptRequest.triggered.Done()
+	return nil, errors.New("timeout")
+
+case upgraderResponse := <-acceptRequest.upgraderResponseChannel:
+	listener.pool.RemoveItems(true, acceptRequest)
+	acceptRequest.triggered.Done()
+	if upgraderResponse.err != nil {
+		return nil, upgraderResponse.err
+	}
+	websocketClient, err := WebsocketClient.New(config, upgraderResponse.websocketConn)
+	if err != nil {
+		listener.ClientsFailed.Add(1)
+		upgraderResponse.websocketConn.Close()
+		return nil, err
+	}
+	listener.ClientsAccepted.Add(1)
+	return websocketClient, nil
+}
+*/
