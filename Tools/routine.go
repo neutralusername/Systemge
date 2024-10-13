ee594aa28b0b8895f2f3c804cd50408eb18f4b23
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-13 21:19:37 +0200

    wjo

--- Tools/routine.go
+++ Tools/routine.go
@@ -1,14 +1,18 @@
 package Tools
 
 import (
+	"errors"
 	"sync"
+	"time"
+
+	"github.com/neutralusername/Systemge/Status"
 )
 
 type RoutineHandler[T any] func(T)
 
 type Routine[T any] struct {
 	status      int
-	statusMutex sync.Mutex
+	statusMutex sync.RWMutex
 
 	delayNs int64
 
@@ -29,5 +33,73 @@ func NewRoutine[T any](handler RoutineHandler[T], maxConcurrentHandlers uint32, 
 		handler:                  handler,
 		acceptRoutineStopChannel: make(chan struct{}),
 		acceptRoutineSemaphore:   semaphore,
+	}
+}
+
+func (routine *Routine[T]) StartRoutine(data T) error {
+	routine.statusMutex.Lock()
+	defer routine.statusMutex.Unlock()
+
+	if routine.status != Status.Stoped {
+		return errors.New("routine already started")
+	}
+
+	routine.status = Status.Started
+
+	routine.acceptRoutineWaitGroup.Add(1)
+	go routine.routine(data)
+
+	return nil
+}
+
+func (routine *Routine[T]) StopRoutine() error {
+	routine.statusMutex.Lock()
+	defer routine.statusMutex.Unlock()
+
+	if routine.status != Status.Started {
+		return errors.New("routine not started")
+	}
+
+	routine.status = Status.Stoped
+
+	close(routine.acceptRoutineStopChannel)
+	routine.acceptRoutineWaitGroup.Wait()
+
+	return nil
+}
+
+func (routine *Routine[T]) IsRoutineRunning() bool {
+	routine.statusMutex.RLock()
+	defer routine.statusMutex.RUnlock()
+
+	return routine.status == Status.Started
+}
+
+func (routine *Routine[T]) routine(data T) {
+	defer routine.acceptRoutineWaitGroup.Done()
+	for {
+		if routine.delayNs > 0 {
+			time.Sleep(time.Duration(routine.delayNs) * time.Nanosecond)
+		}
+		select {
+		case <-routine.acceptRoutineStopChannel:
+			return
+
+		case <-routine.acceptRoutineSemaphore.GetChannel():
+			routine.acceptRoutineWaitGroup.Add(1)
+			go func() {
+				defer func() {
+					routine.acceptRoutineSemaphore.Signal(struct{}{})
+					routine.acceptRoutineWaitGroup.Done()
+				}()
+
+				select {
+				case <-routine.acceptRoutineStopChannel:
+					return
+				default:
+					routine.handler(data)
+				}
+			}()
+		}
 	}
 }
