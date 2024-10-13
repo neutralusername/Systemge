be6dc5b437734a4b2b1f155deaf92d35f9957902
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-13 08:07:58 +0200

    wip

--- Tools/topicManager.go
+++ Tools/topicManager.go
@@ -8,8 +8,8 @@ import (
 	"github.com/neutralusername/Systemge/Config"
 )
 
-type AsyncMessageHandler[O any, C any] func(message O, caller C) error
-type SyncMessageHandler[O any, C any] func(message O, caller C) (O, error)
+type AsyncMessageHandler[O any, C any] func(object O, caller C) error
+type SyncMessageHandler[O any, C any] func(object O, caller C, syncToken string) (O, error)
 
 type TopicHandler[P any, R any] func(P) (R, error)
 type TopicHandlers[P any, R any] map[string]TopicHandler[P, R]
