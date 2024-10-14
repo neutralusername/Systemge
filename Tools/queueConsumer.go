e3e35645808a5cf58e5cb647bb3e6be86e7e15f7
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-14 15:57:21 +0200

    wip

--- Tools/queueConsumer.go
+++ Tools/queueConsumer.go
@@ -1,20 +1,10 @@
 package Tools
 
 import (
-	"sync"
-
 	"github.com/neutralusername/Systemge/Config"
 )
 
 type QueueConsumerFunc[T any] func(T)
-
-type QueueConsumer[T any] struct {
-	status      int
-	statusMutex sync.Mutex
-	waitgroup   sync.WaitGroup
-	stopChannel chan struct{}
-	routine     *Routine
-}
 
 func NewQueueConsumer[T any](config *Config.Routine, queue IQueueConsumption[T], handler QueueConsumerFunc[T]) *Routine {
 	routine := NewRoutine(
