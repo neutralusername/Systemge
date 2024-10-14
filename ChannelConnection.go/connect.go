dd14e70dbd986a8728fd5d136b02c031ab0a6858
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-14 19:35:04 +0200

    wip

--- ChannelConnection.go/connect.go
+++ ChannelConnection.go/connect.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// panic if listener stops while waiting for connection
 func EstablishConnection[T any](connectionChannel ConnectionChannel[T], timeoutNs int64) (*ChannelConnection[T], error) {
 	connectionRequest := &ConnectionRequest[T]{
 		SendToListener:      make(chan T),
