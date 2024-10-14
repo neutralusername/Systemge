5d2341b8651002b883c57e4a7040edee5f37c16c
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-14 17:43:51 +0200

    wip

--- TcpSystemgeConnect/connect.go
+++ TcpSystemgeConnect/connect.go
@@ -2,17 +2,14 @@ package TcpSystemgeConnect
 
 import (
 	"errors"
-	"net"
 
 	"github.com/neutralusername/Systemge/Config"
-	"github.com/neutralusername/Systemge/Event"
-	"github.com/neutralusername/Systemge/Message"
 	"github.com/neutralusername/Systemge/SystemgeConnection"
 	"github.com/neutralusername/Systemge/Tcp"
 	"github.com/neutralusername/Systemge/TcpSystemgeConnection"
 )
 
-func EstablishConnection(config *Config.TcpSystemgeConnection, tcpClientConfig *Config.TcpClient, clientName string, maxServerNameLength int, eventHandler Event.Handler) (SystemgeConnection.SystemgeConnection, error) {
+func EstablishConnection(config *Config.TcpSystemgeConnection, tcpClientConfig *Config.TcpClient) (SystemgeConnection.SystemgeConnection, error) {
 	if config == nil {
 		return nil, errors.New("config is nil")
 	}
@@ -24,7 +21,7 @@ func EstablishConnection(config *Config.TcpSystemgeConnection, tcpClientConfig *
 	if err != nil {
 		return nil, err
 	}
-	connection, err := clientHandshake(config, clientName, maxServerNameLength, netConn, eventHandler)
+	connection, err := TcpSystemgeConnection.New(config, netConn)
 	if err != nil {
 		netConn.Close()
 		return nil, err
@@ -32,41 +29,38 @@ func EstablishConnection(config *Config.TcpSystemgeConnection, tcpClientConfig *
 	return connection, nil
 }
 
-func clientHandshake(config *Config.TcpSystemgeConnection, clientName string, maxServerNameLength int, netConn net.Conn, eventHandler Event.Handler) (*TcpSystemgeConnection.TcpSystemgeConnection, error) {
-	_, err := Tcp.Write(netConn, Message.NewAsync(Message.TOPIC_NAME, clientName).Serialize(), config.TcpSendTimeoutMs)
-	if err != nil {
-		return nil, err
-	}
-	messageReceiver := Tcp.NewBufferedMessageReader(netConn, config.IncomingMessageByteLimit, config.TcpReceiveTimeoutMs, config.TcpBufferBytes)
-	messageBytes, err := messageReceiver.ReadNextMessage()
-	if err != nil {
-		return nil, err
-	}
-	if len(messageBytes) == 0 {
-		return nil, errors.New("received empty message")
-	}
-	filteresMessageBytes := []byte{}
-	for _, b := range messageBytes {
-		if b == Tcp.HEARTBEAT {
-			continue
-		}
-		if b == Tcp.ENDOFMESSAGE {
-			continue
-		}
-		filteresMessageBytes = append(filteresMessageBytes, b)
-	}
-	message, err := Message.Deserialize(filteresMessageBytes, "")
-	if err != nil {
-		return nil, err
-	}
-	if message.GetTopic() != Message.TOPIC_NAME {
-		return nil, errors.New("expected \"" + Message.TOPIC_NAME + "\" message, but got \"" + message.GetTopic() + "\" message")
-	}
-	if maxServerNameLength > 0 && len(message.GetPayload()) > maxServerNameLength {
-		return nil, errors.New("server name is too long")
-	}
-	if message.GetPayload() == "" {
-		return nil, errors.New("server did not respond with a name")
-	}
-	return TcpSystemgeConnection.New(message.GetPayload(), config, netConn, messageReceiver, eventHandler)
+/* _, err := Tcp.Write(netConn, Message.NewAsync(Message.TOPIC_NAME).Serialize(), config.TcpSendTimeoutMs)
+if err != nil {
+	return nil, err
 }
+messageReceiver := Tcp.NewBufferedMessageReader(netConn, config.IncomingMessageByteLimit, config.TcpReceiveTimeoutMs, config.TcpBufferBytes)
+messageBytes, err := messageReceiver.ReadNextMessage()
+if err != nil {
+	return nil, err
+}
+if len(messageBytes) == 0 {
+	return nil, errors.New("received empty message")
+}
+filteresMessageBytes := []byte{}
+for _, b := range messageBytes {
+	if b == Tcp.HEARTBEAT {
+		continue
+	}
+	if b == Tcp.ENDOFMESSAGE {
+		continue
+	}
+	filteresMessageBytes = append(filteresMessageBytes, b)
+}
+message, err := Message.Deserialize(filteresMessageBytes, "")
+if err != nil {
+	return nil, err
+}
+if message.GetTopic() != Message.TOPIC_NAME {
+	return nil, errors.New("expected \"" + Message.TOPIC_NAME + "\" message, but got \"" + message.GetTopic() + "\" message")
+}
+if maxServerNameLength > 0 && len(message.GetPayload()) > maxServerNameLength {
+	return nil, errors.New("server name is too long")
+}
+if message.GetPayload() == "" {
+	return nil, errors.New("server did not respond with a name")
+} */
