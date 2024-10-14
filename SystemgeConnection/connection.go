70250ef027ac1fa4012428be2574035e9783c089
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-14 20:16:53 +0200

    wip

--- SystemgeConnection/connection.go
+++ SystemgeConnection/connection.go
@@ -6,12 +6,24 @@ import (
 )
 
 type SystemgeConnection interface {
+	Close() error
+	GetInstanceId() string
 	GetAddress() string
 	GetStatus() int
-	Close() error
 	GetCloseChannel() <-chan bool
 
+	/* 	StartReadRoutine(uint32, int64, int64, Tools.ReadHandler[O, C]) error
+	   	StopReadRoutine() error
+	   	IsReadRoutineRunning() bool */
+
+	SetReadDeadline(uint64)
+	SetWriteDeadline(uint64)
+
+	Read() ([]byte, error)
+	ReadTimeout(uint64) ([]byte, error)
+
 	Write([]byte) error
+	WriteTimeout([]byte, uint64) error
 
 	GetDefaultCommands() Commands.Handlers
 
