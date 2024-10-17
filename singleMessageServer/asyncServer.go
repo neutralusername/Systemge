0c4cf0f403c462d44503e35204d3f20a78c291dd
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-17 19:45:29 +0200

    wip

--- singleMessageServer/asyncServer.go
+++ singleMessageServer/asyncServer.go
@@ -7,6 +7,32 @@ import (
 	"github.com/neutralusername/systemge/tools"
 )
 
-func NewSingleMessageServerAsync[B any](messageHandlers systemge.AsyncMessageHandlers[B], listener systemge.Listener[B, systemge.Connection[B]], routineConfig *configs.Routine) *singleRequestServer.SingleRequestServerAsync[*tools.IMessage] {
-
+func NewSingleMessageServerAsync[B any](
+	config *configs.SingleRequestServerAsync,
+	routineConfig *configs.Routine,
+	messageHandlers systemge.AsyncMessageHandlers[B],
+	listener systemge.Listener[B, systemge.Connection[B]],
+	acceptHandler tools.AcceptHandlerWithError[systemge.Connection[B]],
+	readHandler tools.ReadHandlerWithError[B, systemge.Connection[B]],
+	messageHandler systemge.AsyncMessageHandler[B],
+	deserialize func(B) (tools.IMessage, error),
+) (*singleRequestServer.SingleRequestServerAsync[B], error) {
+	wrapperReadHandler := func(data B, connection systemge.Connection[B]) {
+		if err := readHandler(data, connection); err != nil {
+			connection.Close()
+		}
+		message, err := deserialize(data)
+		if err != nil {
+			connection.Close()
+			return
+		}
+		// handle message
+	}
+	return singleRequestServer.NewSingleRequestServerAsync(
+		config,
+		routineConfig,
+		listener,
+		acceptHandler,
+		wrapperReadHandler,
+	)
 }
