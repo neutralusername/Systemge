eb802e2b9e124d343206accdbdb9b4ad582f1aba
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-17 10:05:45 +0200

    wip

--- Config/systemge.go
+++ Config/systemge.go
@@ -53,27 +53,6 @@ func UnmarshalTcpSystemgeConnection(data string) *TcpConnection {
 	return &tcpSystemgeConnection
 }
 
-type HTTPServer struct {
-	TcpServerConfig *TcpServer `json:"tcpServerConfig"` // *required*
-
-	HttpErrorLogPath string `json:"httpErrorPath"` // *optional* (logged to standard output if empty)
-
-	DelayNs             int64 `json:"delayNs"`             // default: 0 (no delay)
-	MaxHeaderBytes      int   `json:"maxHeaderBytes"`      // default: <=0 == 1 MB (whichever value you choose, golangs http package will add 4096 bytes on top of it....)
-	ReadHeaderTimeoutMs int   `json:"readHeaderTimeoutMs"` // default: 0 (no timeout)
-	WriteTimeoutMs      int   `json:"writeTimeoutMs"`      // default: 0 (no timeout)
-	MaxBodyBytes        int64 `json:"maxBodyBytes"`        // default: 0 (no limit)
-}
-
-func UnmarshalHTTPServer(data string) *HTTPServer {
-	var http HTTPServer
-	err := json.Unmarshal([]byte(data), &http)
-	if err != nil {
-		return nil
-	}
-	return &http
-}
-
 type WebsocketListener struct {
 	TcpServerConfig *TcpServer `json:"tcpServerConfig"` // *required*
 	Pattern         string     `json:"pattern"`         // *required* (the pattern that the underlying http server will listen to) (e.g. "/ws")
