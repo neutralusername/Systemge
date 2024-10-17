d253e4336c7272888c441b6f7946f8aa25d258ed
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-17 20:33:41 +0200

    wip

--- BrokerResolver/resolver.go
+++ BrokerResolver/resolver.go
@@ -30,24 +30,33 @@ type Resolver[B any] struct {
 }
 
 func New[B any](
-	TopicClientConfigs map[string]*configs.TcpClient,
+	topicClientConfigs map[string]*configs.TcpClient,
 	singleRequestServerConfig *configs.SingleRequestServerSync,
 	routineConfig *configs.Routine,
 	listener systemge.Listener[B, systemge.Connection[B]],
 	acceptHandler tools.AcceptHandlerWithError[systemge.Connection[B]],
-	readHandler tools.ReadHandlerWithResult[B, systemge.Connection[B]],
+	readHandler tools.ReadHandlerWithError[B, systemge.Connection[B]],
 ) (*Resolver[B], error) {
 
-	singleRequestServerSync, err := singleRequestServer.NewSingleRequestServerSync(singleRequestServerConfig, routineConfig, listener, acceptHandler, readHandler)
+	readHandlerWrapper := func(data B, connection systemge.Connection[B]) (B, error) {
+		if err := readHandler(data, connection); err != nil {
+			var nilValue B
+			return nilValue, err
+		}
+
+	}
+
+	singleRequestServerSync, err := singleRequestServer.NewSingleRequestServerSync(singleRequestServerConfig, routineConfig, listener, acceptHandler, readHandlerWrapper)
 	if err != nil {
 		return nil, err
 	}
 
 	resolver := &Resolver[B]{
 		topicTcpClientConfigs: make(map[string]*configs.TcpClient),
+		singleRequestServer:   *singleRequestServerSync,
 	}
 
-	for topic, tcpClientConfig := range TopicClientConfigs {
+	for topic, tcpClientConfig := range topicClientConfigs {
 		normalizedAddress, err := helpers.NormalizeAddress(tcpClientConfig.Address)
 		if err != nil {
 			return nil, err
