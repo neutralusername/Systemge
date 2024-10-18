36c7032e664f0bbfe9f66edc5aaf84e26634fe4f
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-18 10:23:23 +0200

    wip

--- connectionTcp/read.go
+++ connectionTcp/read.go
@@ -6,6 +6,34 @@ import (
 	"github.com/neutralusername/systemge/helpers"
 )
 
+func (client *TcpConnection) ReadChannel(stopChannel <-chan struct{}) <-chan []byte {
+	client.readMutex.Lock()
+	defer client.readMutex.Unlock()
+
+	go func() {
+		for {
+			select {
+			case <-stopChannel:
+				client.SetReadDeadline(1)
+				return
+			}
+		}
+	}()
+
+	resultChannel := make(chan []byte)
+	go func() {
+		defer close(resultChannel)
+
+		bytes, err := client.Read(0)
+		if err != nil {
+			return
+		}
+		resultChannel <- bytes
+	}()
+
+	return resultChannel
+}
+
 func (client *TcpConnection) Read(timeoutNs int64) ([]byte, error) {
 	client.readMutex.Lock()
 	defer client.readMutex.Unlock()
