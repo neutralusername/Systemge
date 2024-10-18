059cd236220dd08a01f9fb57c8258a29ac70d2c8
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-18 10:33:26 +0200

    wip

--- connectionTcp/read.go
+++ connectionTcp/read.go
@@ -6,66 +6,7 @@ import (
 	"github.com/neutralusername/systemge/helpers"
 )
 
-/*
-func (client *TcpConnection) ReadChannel(stopChannel <-chan struct{}) ([]byte, error) {
-	client.readMutex.Lock()
-	defer client.readMutex.Unlock()
-
-	doneChannel := make(chan struct{})
-	go func() {
-		select {
-		case <-stopChannel:
-			client.SetReadDeadline(1)
-			return
-		case <-doneChannel:
-			return
-		}
-	}()
-
-	bytes, err := client.Read(0)
-	close(doneChannel)
-	if err != nil {
-		return nil, err
-	}
-	return bytes, nil
-}
-*/
-
-/*
-func (client *TcpConnection) ReadChannel(stopChannel <-chan struct{}) <-chan []byte {
-	client.readMutex.Lock()
-	defer client.readMutex.Unlock()
-
-	resultChannel := make(chan []byte)
-	doneChannel := make(chan struct{})
-	go func() {
-		defer func() {
-			close(resultChannel)
-			close(doneChannel)
-		}()
-
-		bytes, err := client.Read(0)
-		if err != nil {
-			return
-		}
-		resultChannel <- bytes
-	}()
-
-	go func() {
-		select {
-		case <-stopChannel:
-			client.SetReadDeadline(1)
-			return
-		case <-doneChannel:
-			return
-		}
-	}()
-
-	return resultChannel
-}
-*/
-
-func (client *TcpConnection) ReadChannel() <-chan []byte { // this should be the one with the most generic usage (have this returned channel and ur stop channel in a select then call SetDeadline as needed)
+func (client *TcpConnection) ReadChannel() <-chan []byte {
 	client.readMutex.Lock()
 	defer client.readMutex.Unlock()
 
