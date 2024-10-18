bd8cffa15c14137fe0d0c8f0b7f46628876e0629
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-18 11:15:25 +0200

    wip

--- listenerTcp/accept.go
+++ listenerTcp/accept.go
@@ -1,6 +1,7 @@
 package listenerTcp
 
 import (
+	"errors"
 	"net"
 	"time"
 
@@ -8,14 +9,31 @@ import (
 	"github.com/neutralusername/systemge/systemge"
 )
 
+func (listener *TcpListener) AcceptChannel() <-chan systemge.Connection[[]byte] {
+	resultChannel := make(chan systemge.Connection[[]byte])
+	go func() {
+		defer close(resultChannel)
+
+		connectionRequest, err := listener.Accept(0)
+		if err != nil {
+			return
+		}
+		resultChannel <- connectionRequest
+	}()
+
+	return resultChannel
+}
+
 func (listener *TcpListener) Accept(timeoutNs int64) (systemge.Connection[[]byte], error) {
+	listener.SetAcceptTimeout(timeoutNs)
 
 	l := listener.tcpListener
-	tcpListener := l.(*net.TCPListener)
-	tcpListener.SetDeadline(time.Now().Add(time.Duration(timeoutNs)))
+	if l == nil {
+		return nil, errors.New("tcpSystemgeListener is not started")
+	}
 
-	if listener.tlsListener != nil {
-		l = listener.tlsListener
+	if tlsListener := listener.tlsListener; tlsListener != nil {
+		l = tlsListener
 	}
 
 	netConn, err := l.Accept()
@@ -33,4 +51,13 @@ func (listener *TcpListener) Accept(timeoutNs int64) (systemge.Connection[[]byte
 
 	listener.ClientsAccepted.Add(1)
 	return tcpSystemgeConnection, nil
+}
+
+func (listener *TcpListener) SetAcceptTimeout(timeoutNs int64) {
+	l := listener.tcpListener
+	if l == nil {
+		return
+	}
+	tcpListener := l.(*net.TCPListener)
+	tcpListener.SetDeadline(time.Now().Add(time.Duration(timeoutNs)))
 }
