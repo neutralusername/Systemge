8f8d4023424bb25b2c0cfb5259581ede9d37e6a6
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-18 11:18:05 +0200

    wip

---
+++ tools/channelCall.go
@@ -0,0 +1,16 @@
+package tools
+
+func CallChannel[T any](f func() (T, error)) <-chan T {
+	resultChannel := make(chan T)
+	go func() {
+		defer close(resultChannel)
+
+		result, err := f()
+		if err != nil {
+			return
+		}
+		resultChannel <- result
+	}()
+
+	return resultChannel
+}
