5c13b8d33c6eeb9b1da73883d3abc3569448c029
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-18 12:31:02 +0200

    wip

---
+++ serviceAccepter/server.go
@@ -0,0 +1,167 @@
+package serviceAccepter
+
+import (
+	"encoding/json"
+	"sync/atomic"
+
+	"github.com/neutralusername/systemge/configs"
+	"github.com/neutralusername/systemge/helpers"
+	"github.com/neutralusername/systemge/status"
+	"github.com/neutralusername/systemge/systemge"
+	"github.com/neutralusername/systemge/tools"
+)
+
+type AccepterServer[D any] struct {
+	listener      systemge.Listener[D, systemge.Connection[D]]
+	acceptRoutine *tools.Routine
+
+	AcceptHandler tools.AcceptHandlerWithError[systemge.Connection[D]]
+	ReadHandler   tools.ReadHandlerWithResult[D, systemge.Connection[D]]
+
+	// metrics
+
+	SucceededAccepts atomic.Uint64
+	FailedAccepts    atomic.Uint64
+}
+
+func NewAccepterServer[D any](
+	config *configs.SingleRequestServerSync,
+	routineConfig *configs.Routine,
+	listener systemge.Listener[D, systemge.Connection[D]],
+	acceptHandler tools.AcceptHandlerWithError[systemge.Connection[D]],
+) (*AccepterServer[D], error) {
+
+	server := &AccepterServer[D]{
+		listener:      listener,
+		AcceptHandler: acceptHandler,
+	}
+
+	server.acceptRoutine = tools.NewRoutine(
+		func(stopChannel <-chan struct{}) {
+			select {
+			case <-stopChannel:
+				listener.SetAcceptDeadline(1)
+				// routine was stopped
+				server.FailedAccepts.Add(1)
+				return
+
+			case <-listener.GetStopChannel():
+				server.acceptRoutine.Stop()
+				// listener was stopped
+				server.FailedAccepts.Add(1)
+				return
+
+			case connection, ok := <-helpers.ChannelCall(func() (systemge.Connection[D], error) { return listener.Accept(config.AcceptTimeoutNs) }):
+				if !ok {
+					// do smthg with the error
+					server.FailedAccepts.Add(1)
+					return
+				}
+
+				if err := server.AcceptHandler(connection); err != nil {
+					connection.Close()
+					// do smthg with the error
+					server.FailedAccepts.Add(1)
+					return
+				}
+				server.SucceededAccepts.Add(1)
+			}
+		},
+		routineConfig,
+	)
+	return server, nil
+}
+
+func (server *AccepterServer[D]) GetRoutine() *tools.Routine {
+	return server.acceptRoutine
+}
+
+func (server *AccepterServer[D]) CheckMetrics() tools.MetricsTypes {
+	metricsTypes := tools.NewMetricsTypes()
+	metricsTypes.AddMetrics("single_request_server_sync", tools.NewMetrics(
+		map[string]uint64{
+			"succeededAccepts": server.SucceededAccepts.Load(),
+			"failedAccepts":    server.FailedAccepts.Load(),
+		},
+	))
+	metricsTypes.Merge(server.listener.CheckMetrics())
+	return metricsTypes
+}
+func (server *AccepterServer[D]) GetMetrics() tools.MetricsTypes {
+	metricsTypes := tools.NewMetricsTypes()
+	metricsTypes.AddMetrics("single_request_server_sync", tools.NewMetrics(
+		map[string]uint64{
+			"succeededAccepts": server.SucceededAccepts.Swap(0),
+			"failedAccepts":    server.FailedAccepts.Swap(0),
+		},
+	))
+	metricsTypes.Merge(server.listener.GetMetrics())
+	return metricsTypes
+}
+
+func (server *AccepterServer[D]) GetDefaultCommands() tools.CommandHandlers {
+	commands := tools.CommandHandlers{}
+	commands["start"] = func(args []string) (string, error) {
+		err := server.GetRoutine().Start()
+		if err != nil {
+			return "", err
+		}
+		return "success", nil
+	}
+	commands["stop"] = func(args []string) (string, error) {
+		err := server.GetRoutine().Stop()
+		if err != nil {
+			return "", err
+		}
+		return "success", nil
+	}
+	commands["getStatus"] = func(args []string) (string, error) {
+		return status.ToString(server.acceptRoutine.GetStatus()), nil
+	}
+	commands["checkMetrics"] = func(args []string) (string, error) {
+		metrics := server.CheckMetrics()
+		json, err := json.Marshal(metrics)
+		if err != nil {
+			return "", err
+		}
+		return string(json), nil
+	}
+	commands["getMetrics"] = func(args []string) (string, error) {
+		metrics := server.GetMetrics()
+		json, err := json.Marshal(metrics)
+		if err != nil {
+			return "", err
+		}
+		return string(json), nil
+	}
+	listenerCommands := server.listener.GetDefaultCommands()
+	for key, value := range listenerCommands {
+		commands["listener_"+key] = value
+	}
+	return commands
+}
+
+/*
+select {
+case <-stopChannel:
+	connection.SetReadDeadline(1)
+	// routine was stopped
+	server.FailedReads.Add(1)
+	return
+
+case <-listener.GetStopChannel():
+	server.acceptRoutine.Stop()
+	// listener was stopped
+	server.FailedReads.Add(1)
+	return
+
+case data, ok := <-helpers.ChannelCall(func() (D, error) { return connection.Read(config.ReadTimeoutNs) }):
+	if !ok {
+		// do smthg with the error
+		server.FailedReads.Add(1)
+		return
+	}
+	server.readHandler(data, connection)
+	server.SucceededReads.Add(1)
+}
+*/
