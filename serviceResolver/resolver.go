4cdf09890c092f0db07b106708b2e346a243c89e
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-18 21:50:17 +0200

    wip

--- serviceResolver/resolver.go
+++ serviceResolver/resolver.go
@@ -11,16 +11,15 @@ import (
 	"github.com/neutralusername/systemge/tools"
 )
 
-func New[D any, O any](
+func New[D any](
 	listener systemge.Listener[D, systemge.Connection[D]],
 	accepterConfig *configs.AccepterServer,
 	readerConfig *configs.ReaderServerSync,
 	routineConfig *configs.Routine,
 	acceptHandler tools.AcceptHandlerWithError[systemge.Connection[D]],
 	readHandler tools.ReadHandlerWithError[D, systemge.Connection[D]],
-	topicObject map[string]O,
+	topicData map[string]D,
 	deserializeTopic func(D, systemge.Connection[D]) (string, error), // responsible for retrieving the topic
-	serializeObject func(O) (D, error),
 ) (*serviceAccepter.Accepter[D], error) {
 
 	return serviceSingleRequest.NewSync(
@@ -30,19 +29,19 @@ func New[D any, O any](
 		routineConfig,
 		true,
 		acceptHandler,
-		func(closeChannel <-chan struct{}, data D, connection systemge.Connection[D]) (D, error) {
-			if err := readHandler(closeChannel, data, connection); err != nil {
-				return helpers.GetNilValue(data), err
+		func(closeChannel <-chan struct{}, incomingData D, connection systemge.Connection[D]) (D, error) {
+			if err := readHandler(closeChannel, incomingData, connection); err != nil {
+				return helpers.GetNilValue(incomingData), err
 			}
-			topic, err := deserializeTopic(data, connection)
+			topic, err := deserializeTopic(incomingData, connection)
 			if err != nil {
-				return helpers.GetNilValue(data), err
+				return helpers.GetNilValue(incomingData), err
 			}
-			object, ok := topicObject[topic]
+			outgoingData, ok := topicData[topic]
 			if !ok {
-				return helpers.GetNilValue(data), errors.New("topic not found")
+				return helpers.GetNilValue(incomingData), errors.New("topic not found")
 			}
-			return serializeObject(object)
+			return outgoingData, nil
 		},
 	)
 }
