d8b123caf7655c59984f62f1a5c909883d7f74f3
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-22 20:48:19 +0200

    wip

--- serverAcceptSession/server.go
+++ serverAcceptSession/server.go
@@ -1,12 +1,1 @@
 package serverAcceptSession
-
-import (
-	"github.com/neutralusername/systemge/systemge"
-	"github.com/neutralusername/systemge/tools"
-)
-
-func New[D any](
-	sessionManager *tools.SessionManager,
-) func(connection systemge.Connection[D]) error {
-
-}
