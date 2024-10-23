c067da149db422fee6b823d026ca09aa96f4a4dd
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-23 07:04:57 +0200

    wip

--- serverMessage/server.go
+++ serverMessage/server.go
@@ -3,11 +3,13 @@ package serverMessage
 import (
 	"github.com/neutralusername/systemge/server"
 	"github.com/neutralusername/systemge/systemge"
+	"github.com/neutralusername/systemge/tools"
 )
 
 func New[O any](
 	asyncMessageHandlers systemge.AsyncMessageHandlers[O],
 	syncMessageHandlers systemge.SyncMessageHandlers[O],
+	acceptHandler tools.AcceptHandlerWithError[O],
 ) *server.Server[O] {
 
 }
