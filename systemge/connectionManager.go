6e453491e5e8b87b4b660de192e0daacf9213845
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-23 09:07:08 +0200

    wip

--- systemge/connectionManager.go
+++ systemge/connectionManager.go
@@ -2,6 +2,8 @@ package systemge
 
 import "github.com/neutralusername/systemge/tools"
 
+// todo think of a way to integrate this with server for effective and user friendly typed messaging
+
 type ConnectionManager[D any] struct {
 	*tools.ObjectManager[Connection[D]]
 }
