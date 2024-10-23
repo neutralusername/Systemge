c267d759b9f87edcc340051dbf7adfbf852b71de
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-23 08:50:49 +0200

    wip

--- systemge/connectionManager.go
+++ systemge/connectionManager.go
@@ -6,13 +6,6 @@ type ConnectionManager[D any] struct {
 	*tools.ObjectManager[Connection[D]]
 }
 
-func (connectionManager *ConnectionManager[D]) Write(data D, timeoutNs int64, connections ...Connection[D]) {
-	if len(connections) == 0 {
-		connections = connectionManager.GetBulk()
-	}
-	MultiWrite(data, timeoutNs, connections...)
-}
-
-func (connectionManager *ConnectionManager[D]) WriteId(data D, timeoutNs int64, ids ...string) {
+func (connectionManager *ConnectionManager[D]) Write(data D, timeoutNs int64, ids ...string) {
 	MultiWrite(data, timeoutNs, connectionManager.GetBulk(ids...)...)
 }
