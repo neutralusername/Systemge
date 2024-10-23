672111883bae85acfd939ae6968a37fb3857ebf7
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-23 09:23:06 +0200

    wip

--- tools/queueConsumer.go
+++ tools/queueConsumer.go
@@ -4,6 +4,13 @@ import (
 	"github.com/neutralusername/systemge/configs"
 )
 
+type IQueueConsumer[T any] interface {
+	Pop() (T, error)
+	PopBlocking() T
+	PopChannel() <-chan T
+	Len() int
+}
+
 type QueueConsumerFunc[T any] func(T)
 
 func NewQueueConsumer[T any](config *configs.Routine, queue IQueueConsumer[T], handler QueueConsumerFunc[T]) (*Routine, error) {
--- tools/queueConsumption.go
+++
@@ -1,8 +0,0 @@
-package tools
-
-type IQueueConsumer[T any] interface {
-	Pop() (T, error)
-	PopBlocking() T
-	PopChannel() <-chan T
-	Len() int
-}
