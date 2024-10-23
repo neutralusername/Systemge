39251547854d43a44a7d69455b6eb38c3a8b392e
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-23 11:55:44 +0200

    wip

--- systemge/connectionManager.go
+++ systemge/connectionManager.go
@@ -8,6 +8,14 @@ type ConnectionManager[T any] struct {
 	*tools.ObjectManager[Connection[T]]
 }
 
+func NewConnectionManager[T any](
+	objectManager *tools.ObjectManager[Connection[T]],
+) *ConnectionManager[T] {
+	return &ConnectionManager[T]{
+		ObjectManager: objectManager,
+	}
+}
+
 func (connectionManager *ConnectionManager[T]) Write(data T, timeoutNs int64, ids ...string) {
 	MultiWrite(data, timeoutNs, connectionManager.GetBulk(ids...))
 }
