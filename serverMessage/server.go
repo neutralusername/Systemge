b6fffdf89f91ab733a45aa49e502ece55a6499a3
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-23 07:05:59 +0200

    wip

--- serverMessage/server.go
+++ serverMessage/server.go
@@ -1,15 +1,21 @@
 package serverMessage
 
 import (
+	"github.com/neutralusername/systemge/configs"
 	"github.com/neutralusername/systemge/server"
 	"github.com/neutralusername/systemge/systemge"
 	"github.com/neutralusername/systemge/tools"
 )
 
-func New[O any](
-	asyncMessageHandlers systemge.AsyncMessageHandlers[O],
-	syncMessageHandlers systemge.SyncMessageHandlers[O],
-	acceptHandler tools.AcceptHandlerWithError[O],
-) *server.Server[O] {
+func New[D any](
+	listener systemge.Listener[D, systemge.Connection[D]],
+	accepterConfig *configs.Accepter,
+	accepterRoutineConfig *configs.Routine,
+	readerServerAsyncConfig *configs.ReaderAsync,
+	readerRoutineConfig *configs.Routine,
+	asyncMessageHandlers systemge.AsyncMessageHandlers[D],
+	syncMessageHandlers systemge.SyncMessageHandlers[D],
+	acceptHandler tools.AcceptHandlerWithError[D],
+) *server.Server[D] {
 
 }
