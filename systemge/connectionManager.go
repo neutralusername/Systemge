ae39197dd815aea8838835518d25f2a2ae37037c
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-23 07:54:46 +0200

    wip

---
+++ systemge/connectionManager.go
@@ -0,0 +1,82 @@
+package systemge
+
+import (
+	"errors"
+	"math"
+	"sync"
+
+	"github.com/neutralusername/systemge/tools"
+)
+
+type ConnectionManager[D any] struct {
+	sessionIdLength   uint32
+	sessionIdAlphabet string
+	sessionCap        int
+
+	sessionMutex sync.RWMutex
+	sessions     map[string]Connection[D]
+	connections  map[Connection[D]]string
+}
+
+func NewSessionManager[D any](sessionIdLength uint32, sessionIdAlphabet string) *ConnectionManager[D] {
+
+	return &ConnectionManager[D]{
+		sessionIdLength:   sessionIdLength,
+		sessionIdAlphabet: sessionIdAlphabet,
+		sessionCap:        int(math.Pow(float64(len(sessionIdAlphabet)), float64(sessionIdLength)) * 0.9),
+
+		sessions:    make(map[string]Connection[D]),
+		connections: make(map[Connection[D]]string),
+	}
+}
+
+func (manager *ConnectionManager[D]) CreateSession(connection Connection[D]) (string, error) {
+	if len(manager.sessions) >= manager.sessionCap {
+		manager.sessionMutex.Unlock()
+		return "", errors.New("maximum number of sessions reached")
+	}
+
+	id := tools.GenerateRandomString(manager.sessionIdLength, manager.sessionIdAlphabet)
+	for {
+		if _, ok := manager.sessions[id]; !ok {
+			break
+		}
+		id = tools.GenerateRandomString(manager.sessionIdLength, manager.sessionIdAlphabet)
+	}
+
+	manager.sessions[id] = connection
+	manager.connections[connection] = id
+	manager.sessionMutex.Unlock()
+
+	return id, nil
+}
+
+func (manager *ConnectionManager[D]) RemoveSession(sessionId string) error {
+	manager.sessionMutex.Lock()
+	defer manager.sessionMutex.Unlock()
+	connection, ok := manager.sessions[sessionId]
+	if !ok {
+		return errors.New("session not found")
+	}
+	delete(manager.sessions, sessionId)
+	delete(manager.connections, connection)
+	return nil
+}
+
+func (manager *ConnectionManager[D]) GetConnection(id string) Connection[D] {
+	manager.sessionMutex.RLock()
+	defer manager.sessionMutex.RUnlock()
+	if session, ok := manager.sessions[id]; ok {
+		return session
+	}
+	return nil
+}
+
+func (manager *ConnectionManager[D]) GetId(connection Connection[D]) string {
+	manager.sessionMutex.RLock()
+	defer manager.sessionMutex.RUnlock()
+	if id, ok := manager.connections[connection]; ok {
+		return id
+	}
+	return ""
+}
