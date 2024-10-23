6c8b9027e33666a03f4c7a5198b0f14776b1345b
fabian thomas stuck <stuck.fabian@gmail.com>
2024-10-23 18:45:09 +0200

    wip

--- accepter/handlers.go
+++ accepter/handlers.go
@@ -207,14 +207,22 @@ func NewSingleReadSyncHandler[T any](
 // adds connection to connection manager and removes it when connection is closed
 func AcceptConnectionHandler[T any](
 	connectionManager *systemge.ConnectionManager[T],
+	removeOnClose bool,
 ) systemge.AcceptHandlerWithError[T] {
-	return func(connection systemge.Connection[T]) error {
-		_, err := connectionManager.Add(connection)
-		go func() {
-			<-connection.GetCloseChannel()
-			connectionManager.Remove(connection)
-		}()
-		return err
+	if removeOnClose {
+		return func(connection systemge.Connection[T]) error {
+			_, err := connectionManager.Add(connection)
+			go func() {
+				<-connection.GetCloseChannel()
+				connectionManager.Remove(connection)
+			}()
+			return err
+		}
+	} else {
+		return func(connection systemge.Connection[T]) error {
+			_, err := connectionManager.Add(connection)
+			return err
+		}
 	}
 }
 
